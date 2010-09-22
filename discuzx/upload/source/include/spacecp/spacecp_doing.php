57f7b71b00fc18e41f95600d0c4254ec5ca7c7ce
bluelovers <codelovers@users.sourceforge.net>
2010-09-22 21:40:37 +0800

    [home][doing] 更改 回應時的樹型階層判定 [home][doing] 將 message 改為可顯示分行 [home][doing] 增加部分 css class

--- discuzx/upload/source/include/spacecp/spacecp_doing.php
+++ discuzx/upload/source/include/spacecp/spacecp_doing.php
@@ -14,6 +14,25 @@ if(!defined('IN_DISCUZ')) {
 $doid = empty($_GET['doid'])?0:intval($_GET['doid']);
 $id = empty($_GET['id'])?0:intval($_GET['id']);
 
+// bluelovers
+//discuz_core::instance()->debug_log(CURSCRIPT, $_GET);
+
+/**
+ * 回應的最大字數
+ * @var int
+ **/
+$message_strlen = 255;
+
+/**
+ * 回應的最小字數
+ * @var int
+ **/
+$message_strlen_min = 3;
+
+$tree_level = 3;
+
+// bluelovers
+
 if(submitcheck('addsubmit')) {
 
 	$add_doing = 1;
@@ -21,10 +40,13 @@ if(submitcheck('addsubmit')) {
 		showmessage('no_privilege');
 	}
 
+	//實名認證
 	ckrealname('doing');
 
+	//視頻認證
 	ckvideophoto('doing');
 
+	//新用戶見習
 	cknewuser();
 
 	$waittime = interval_check('post');
@@ -32,16 +54,27 @@ if(submitcheck('addsubmit')) {
 		showmessage('operating_too_fast', '', array('waittime' => $waittime));
 	}
 
-	$message = getstr($_POST['message'], 200, 1, 1, 1);
+
+//	$message = getstr($_POST['message'], 200, 1, 1, 1);
+	$message = getstr($_POST['message'], $message_strlen, 1, 1, 1);
 	$message = preg_replace("/\<br.*?\>/i", ' ', $message);
-	if(strlen($message) < 1) {
+//	if(strlen($message) < 1) {
+//		showmessage('should_write_that');
+//	}
+
+	// bluelovers
+	$message = trim($message, '　');
+
+	$_message = trim(preg_replace("/^@[^\s]*[\s　]*/i", '', $message));
+	if(
+		strlen($message) < $message_strlen_min
+		|| strlen($_message) < $message_strlen_min
+	) {
 		showmessage('should_write_that');
 	}
 
-	$message = censor($message, NULL, TRUE);
-	if(is_array($message) && $message['message']) {
-		showmessage('do_success', dreferer(), array('message'=>$message['message']));
-	}
+	$message = trim(preg_replace("/(@[^\s]*)[\s　]*/i", '\\1 ', $message), '　');
+	// bluelovers
 
 	if(censormod($message) || $_G['group']['allowdoingmod']) {
 		$doing_status = 1;
@@ -91,6 +124,7 @@ if(submitcheck('addsubmit')) {
 		DB::insert('home_feed', $feedarr);
 	}
 
+	//統計
 	require_once libfile('function/stat');
 	updatestat('doing');
 	DB::update('common_member_status', array('lastpost' => $_G['timestamp']), array('uid' => $_G['uid']));
@@ -106,24 +140,50 @@ if(submitcheck('addsubmit')) {
 		showmessage('no_privilege');
 	}
 
+	//實名認證
 	ckrealname('doing');
 
+	//新用戶見習
 	cknewuser();
 
+	//判斷是否操作太快
 	$waittime = interval_check('post');
 	if($waittime > 0) {
 		showmessage('operating_too_fast', '', array('waittime' => $waittime));
 	}
 
-	$message = getstr($_POST['message'], 200, 1, 1, 1);
+//	$message = getstr($_POST['message'], 200, 1, 1, 1);
+	$message = getstr($_POST['message'], $message_strlen, 1, 1, 1);
 	$message = preg_replace("/\<br.*?\>/i", ' ', $message);
-	if(strlen($message) < 1) {
+
+//	if(strlen($message) < 1) {
+//		showmessage('should_write_that');
+//	}
+
+	// bluelovers
+	$message = trim($message, '　');
+
+	$_message = trim(preg_replace("/@[^\s]*[\s　]*/i", '', $message), '　');
+
+//	discuz_core::instance()->debug_log('should_write_that', array($message, $_message));
+
+	if(
+		strlen($message) < $message_strlen_min
+		|| strlen($_message) < $message_strlen_min
+	) {
 		showmessage('should_write_that');
 	}
+	$message = trim(preg_replace("/(@[^\s]*)[\s　]*/i", '\\1 ', $message), '　');
+	// bluelovers
+
 	$message = censor($message);
 
-
 	$updo = array();
+
+	// bluelovers
+	$topupdo = array();
+	// bluelovers
+
 	if($id) {
 		$query = DB::query("SELECT * FROM ".DB::table('home_docomment')." WHERE id='$id'");
 		$updo = DB::fetch($query);
@@ -131,10 +191,19 @@ if(submitcheck('addsubmit')) {
 	if(empty($updo) && $doid) {
 		$query = DB::query("SELECT * FROM ".DB::table('home_doing')." WHERE doid='$doid'");
 		$updo = DB::fetch($query);
+
+	// bluelovers
+	//TODO: fix here - docomment
+		$topupdo = $updo;
+	} elseif ($id && $updo['upid']) {
+		$topupdo = DB::query_first("SELECT * FROM ".DB::table('home_docomment')." WHERE id='$updo[upid]'");
+	// bluelovers
+
 	}
 	if(empty($updo)) {
 		showmessage('docomment_error');
 	} else {
+		//黑名單
 		if(isblacklist($updo['uid'])) {
 			showmessage('is_blacklist');
 		}
@@ -154,28 +223,47 @@ if(submitcheck('addsubmit')) {
 		'grade' => $updo['grade']+1
 	);
 
-	if($updo['grade'] >= 3) {
+	// bluelovers
+	if ($updo['uid'] == $_G['uid'] && $updo['grade']) {
+		// 如果回應的評論是自己的則保持在同級
 		$setarr['upid'] = $updo['upid'];
+		$setarr['grade'] = $updo['grade'];
+	} elseif (!empty($topupdo) && $topupdo['uid'] == $_G['uid'] && $updo['grade']) {
+		$setarr['upid'] = $topupdo['upid'];
+		$setarr['grade'] = $updo['grade'];
+	}
+	// bluelovers
+
+	//最多層級
+	if($updo['grade'] >= $tree_level) {
+		$setarr['upid'] = $updo['upid'];
+		//更母一個級別
 	}
 
 	$newid = DB::insert('home_docomment', $setarr, 1);
 
+	//更新回複數
 	DB::query("UPDATE ".DB::table('home_doing')." SET replynum=replynum+1 WHERE doid='$updo[doid]'");
 
+	//通知
 	if($updo['uid'] != $_G['uid']) {
 		notification_add($updo['uid'], 'doing', 'doing_reply', array(
 			'url'=>"home.php?mod=space&do=doing&doid=$updo[doid]&highlight=$newid",
 			'from_id'=>$updo['doid'],
 			'from_idtype'=>'doid'));
+
+		//獎勵積分
 		updatecreditbyaction('comment', 0, array(), 'doing'.$updo['doid']);
 	}
 
+	//統計
 	include_once libfile('function/stat');
 	updatestat('docomment');
 	DB::update('common_member_status', array('lastpost' => $_G['timestamp']), array('uid' => $_G['uid']));
 	showmessage('do_success', dreferer(), array('doid' => $updo['doid']));
 }
 
+//刪除
 if($_GET['op'] == 'delete') {
 
 	if(submitcheck('deletesubmit')) {
@@ -184,8 +272,10 @@ if($_GET['op'] == 'delete') {
 			$query = DB::query("SELECT dc.*, d.uid as duid FROM ".DB::table('home_docomment')." dc, ".DB::table('home_doing')." d WHERE dc.id='$id' AND dc.doid=d.doid");
 			if($value = DB::fetch($query)) {
 				if($allowmanage || $value['uid'] == $_G['uid'] || $value['duid'] == $_G['uid'] ) {
+					//更新內容
 					DB::update('home_docomment', array('uid'=>0, 'username'=>'', 'message'=>''), "id='$id'");
 					if($value['uid'] != $_G['uid'] && $value['duid'] != $_G['uid']) {
+						//扣除積分
 						batchupdatecredit('comment', $value['uid'], array(), -1);
 					}
 					DB::query("UPDATE ".DB::table('home_doing')." SET replynum=replynum-'1' WHERE doid='$doid'");
@@ -239,6 +329,19 @@ if($_GET['op'] == 'delete') {
 	}
 } elseif ($_GET['op'] == 'spacenote') {
 	space_merge($space, 'field_home');
+
+// bluelovers
+} elseif ($_GET['op'] == 'docomment') {
+	$query = DB::fetch_first("SELECT dc.* FROM ".DB::table('home_docomment')." dc WHERE dc.id='$id' AND dc.doid='$doid'");
+
+	if (empty($_G['gp_message']) && $query['uid'] != $_G['uid'] && $query['username']) {
+		$_G['gp_message'] = '@'.dhtmlspecialchars($query['username']).' ';
+	} else {
+		$_G['gp_message'] = !empty($_G['gp_message']) ? dhtmlspecialchars($_G['gp_message']) : '';
+	}
+
+// bluelovers
+
 }
 
 include template('home/spacecp_doing');
