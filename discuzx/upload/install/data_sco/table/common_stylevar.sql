b21fe64e65e6e86a6f7aa8b9cea2c2195850b709
bluelovers <codelovers@users.sourceforge.net>
2011-06-24 13:23:42 +0800

    threadtitlefontsize 14px => 12px

--- discuzx/upload/install/data_sco/table/common_stylevar.sql
+++ discuzx/upload/install/data_sco/table/common_stylevar.sql
@@ -1,2 +1,2 @@
 
-UPDATE `pre_common_stylevar` SET substitute = '12px' WHERE variable = 'msgfontsize';
+UPDATE `pre_common_stylevar` SET substitute = '12px' WHERE variable = 'msgfontsize' OR variable = 'threadtitlefontsize';
