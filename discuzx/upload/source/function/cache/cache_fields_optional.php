7d1b7990b250e4253676711c8b603886ecf0e497
bluelovers <codelovers@users.sourceforge.net>
2011-06-24 15:24:22 +0800

    修正 $field['selective'] 不存在於 DX 1.5, DX 2.0
    但是實際上 fields_optional, fields_register, fields_required
    只使用到 $field['fieldid'] 欄位
    其餘皆為使用 profilesetting 內儲存的資料
    但仍有可能有不確定的 BUG 存在
    
    另外 $field['choices'] 欄位與 profilesetting 內的儲存格式也不同
    新版 DX 2.0 的 $field['choices'] 不支援 1=abc 這種格式
    導致舊版升級後 會造成 舊的自訂欄位有關於 choices 類的資料全部無效
    
    profilesetting 內為 DX 1.5, DX 2.0 的格式
    fields_optional, fields_register, fields_required 內為 D 7.2 以前的格式

--- discuzx/upload/source/function/cache/cache_fields_optional.php
+++ discuzx/upload/source/function/cache/cache_fields_optional.php
@@ -17,6 +17,24 @@ function build_cache_fields_optional() {
 
 	while($field = DB::fetch($query)) {
 		$choices = array();
+		// bluelvoers
+		/**
+		 * 修正 $field['selective'] 不存在於 DX 1.5, DX 2.0
+		 *
+		 * 但是實際上 fields_optional, fields_register, fields_required
+		 * 只使用到 $field['fieldid'] 欄位
+		 * 其餘皆為使用 profilesetting 內儲存的資料
+		 * 但仍有可能有不確定的 BUG 存在
+		 *
+		 * 另外 $field['choices'] 欄位與 profilesetting 內的儲存格式也不同
+		 * 新版 DX 2.0 的 $field['choices'] 不支援 1=abc 這種格式
+		 * 導致舊版升級後 會造成 舊的自訂欄位有關於 choices 類的資料全部無效
+		 *
+		 * profilesetting 內為 DX 1.5, DX 2.0 的格式
+		 * fields_optional, fields_register, fields_required 內為 D 7.2 以前的格式
+		 **/
+		$field['selective'] = !empty($field['choices']);
+		// bluelvoers
 		if($field['selective']) {
 			foreach(explode("\n", $field['choices']) as $item) {
 				list($index, $choice) = explode('=', $item);
