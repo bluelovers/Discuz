f40f18aee9a69f8d28acc40a8f97a717a7c009e8
bluelovers <codelovers@users.sourceforge.net>
2011-06-29 20:35:58 +0800

    FIX 後台搜索用戶管理統計信息的鏈接問題

--- discuzx/upload/source/admincp/admincp_index.php
+++ discuzx/upload/source/admincp/admincp_index.php
@@ -4,7 +4,7 @@
  *      [Discuz!] (C)2001-2099 Comsenz Inc.
  *      This is NOT a freeware, use is subject to license terms
  *
- *      $Id: admincp_index.php 21934 2011-04-18 05:38:00Z svn_project_zhangjie $
+ *      $Id: admincp_index.php 23193 2011-06-24 01:46:47Z liulanbo $
  */
 
 if(!defined('IN_DISCUZ') || !defined('IN_ADMINCP')) {
@@ -166,7 +166,7 @@ while($online = DB::fetch($query)) {
 echo '<div id="boardnews"></div>';
 
 echo '<style>.rssbook{margin:8px 0 0 25px;}</style>';
-echo '<script >var nId = "9d541168ad2e0ca633af8218f7d666f41d529a534c29fac0",nWidth="400px",sColor="light",sText="'.cplang('subscribe_comsenz_email').'" ;</script><script src="http://list.qq.com/zh_CN/htmledition/js/qf/page/qfcode.js" charset="gb18030"></script>';
+echo '<script >var nId = "4d1e7b6dd9c5070d1a82aeb8be5e72fc64db42701a1bc4d4",nWidth="400px",sColor="light",sText="'.cplang('subscribe_comsenz_email').'" ;</script><script src="http://list.qq.com/zh_CN/htmledition/js/qf/page/qfcode.js" charset="gb18030"></script>';
 showtableheader('', 'nobottom fixpadding');
 if($membersmod || $threadsmod || $postsmod || $medalsmod || $blogsmod || $picturesmod || $doingsmod || $sharesmod || $commentsmod || $articlesmod || $articlecommentsmod || $topiccommentsmod || $threadsdel || !empty($verify)) {
 	showtablerow('', '', '<h3 class="left margintop">'.cplang('home_mods').': </h3><p class="left difflink">'.
