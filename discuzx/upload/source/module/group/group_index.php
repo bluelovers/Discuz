1467429a67d9bb58e90620ffcb7ebb512bebab08
bluelovers <codelovers@users.sourceforge.net>
2011-07-01 13:13:56 +0800

    del temp code

--- discuzx/upload/source/module/group/group_index.php
+++ discuzx/upload/source/module/group/group_index.php
@@ -154,30 +154,6 @@ if (discuz_core::instance()->plugin_support['Scorpio_Event']) {
 // bluelovers
 
 if(empty($curtype)) {
-
-	// bluelovers
-	// 臨時性解決推薦群組不足的時候自動補充
-	if(empty($curtype)) {
-		$recommend_num = 8;
-		$group_recommend = unserialize($_G['setting']['group_recommend']);
-
-		if (count($group_recommend) < $recommend_num) {
-			$query = DB::query("SELECT f.fid, f.name, ff.description, ff.icon FROM ".DB::table('forum_forum')." f LEFT JOIN ".DB::table('forum_forumfield')." ff USING(fid) WHERE f.status='3' AND f.type='sub'
-			ORDER BY f.commoncredits desc, ff.membernum desc LIMIT $recommend_num");
-			while($row = DB::fetch($query)) {
-				$row['icon'] = get_groupimg($row['icon'], 'icon');
-				if(count($group_recommend) == $recommend_num) {
-					break;
-				} elseif(empty($group_recommend[$row[fid]])) {
-					$group_recommend[$row[fid]] = $row;
-				}
-			}
-
-			$_G['setting']['group_recommend'] = serialize($group_recommend);
-		}
-	}
-	// bluelovers
-
 	include template('diy:group/index');
 } else {
 	if(empty($sgid)) {
