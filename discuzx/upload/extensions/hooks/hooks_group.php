c4416f9a8b3c0f19d01532dac883fc27dad9323e
bluelovers <codelovers@users.sourceforge.net>
2011-07-01 13:01:29 +0800

    if (!discuz_core::instance()->plugin_support['Scorpio_Event']) return false;

--- discuzx/upload/extensions/hooks/hooks_group.php
+++ discuzx/upload/extensions/hooks/hooks_group.php
@@ -10,6 +10,8 @@
 	$Id: $
 */
 
+if (!discuz_core::instance()->plugin_support['Scorpio_Event']) return false;
+
 Scorpio_Hook::add('Dz_module_group_index:Before_template', '_eDz_module_group_index_Before_template');
 
 function _eDz_module_group_index_Before_template($conf) {
