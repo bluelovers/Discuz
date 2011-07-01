f588119afcac109ed3be0573ace81ad97a5fa9ab
bluelovers <codelovers@users.sourceforge.net>
2011-07-01 20:37:51 +0800

    func formatforumdata's $keys add description

--- discuzx/upload/source/function/cache/cache_forums.php
+++ discuzx/upload/source/function/cache/cache_forums.php
@@ -82,7 +82,7 @@ function build_cache_forums() {
 
 function formatforumdata($forum, &$pluginvalue) {
 	static $keys = array('fid', 'type', 'name', 'fup', 'viewperm', 'postperm', 'orderby', 'ascdesc', 'users', 'status',
-		'extra', 'plugin', 'allowpostspecial', 'commentitem');
+		'extra', 'plugin', 'allowpostspecial', 'commentitem', 'description');
 	static $orders = array('lastpost', 'dateline', 'replies', 'views');
 
 	$data = array();
