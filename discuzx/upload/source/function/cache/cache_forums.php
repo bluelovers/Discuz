752fc422fcce58b0c43bf210773678e2a78f8733
bluelovers <codelovers@users.sourceforge.net>
2011-07-01 20:27:15 +0800

    part.b.2

--- discuzx/upload/source/function/cache/cache_forums.php
+++ discuzx/upload/source/function/cache/cache_forums.php
@@ -15,10 +15,10 @@ function build_cache_forums() {
 	$data = array();
 
 	// bluelovers
-	$_query_add = ', ff.description';
+	$_query_add = ', ff.description ';
 	// bluelovers
 
-	$query = DB::query("SELECT f.fid, f.type, f.name, f.fup, f.simple, f.status, f.allowpostspecial, ff.viewperm, ff.formulaperm, ff.viewperm, ff.postperm, ff.replyperm, ff.getattachperm, ff.postattachperm, ff.extra, ff.commentitem, a.uid FROM ".DB::table('forum_forum')." f
+	$query = DB::query("SELECT f.fid, f.type, f.name, f.fup, f.simple, f.status, f.allowpostspecial, ff.viewperm, ff.formulaperm, ff.viewperm, ff.postperm, ff.replyperm, ff.getattachperm, ff.postattachperm, ff.extra, ff.commentitem, a.uid {$_query_add} FROM ".DB::table('forum_forum')." f
 		LEFT JOIN ".DB::table('forum_forumfield')." ff ON ff.fid=f.fid LEFT JOIN ".DB::table('forum_access')." a ON a.fid=f.fid AND a.allowview>'0' WHERE f.status<>'3' ORDER BY f.type, f.displayorder");
 
 	$pluginvalue = $forumlist = array();
