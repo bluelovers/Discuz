9b80a1d35b9e5f269aae360795a9c450639bb03f
bluelovers <codelovers@users.sourceforge.net>
2011-07-01 12:49:17 +0800

    rewrite Scorpio_Hook => Scorpio_Event

--- discuzx/upload/source/module/group/group_index.php
+++ discuzx/upload/source/module/group/group_index.php
@@ -143,7 +143,7 @@ if(!$metadescription) {
 
 // bluelovers
 if (discuz_core::instance()->plugin_support['Scorpio_Event']) {
-	Scorpio_Hook::execute('Dz_module_'.basename(__FILE__, '.php').':Before_template', array(array(
+	Scorpio_Event::instance('Dz_module_'.basename(__FILE__, '.php').':Before_template')->run(array(array(
 		'curtype' => &$curtype,
 	)));
 }
