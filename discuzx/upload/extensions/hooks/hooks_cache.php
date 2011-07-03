0c06e0e8fe92f98572f033027cc14494fdf161e8
bluelovers <codelovers@users.sourceforge.net>
2011-07-03 22:06:46 +0800

    add hooks/hooks_cache.php

---
+++ discuzx/upload/extensions/hooks/hooks_cache.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @author bluelovers
+ */
+
+if (!discuz_core::$plugin_support['Scorpio_Event']) return false;
+
+Scorpio_Hook::add('Class_template::parse_template:Before_addon_tpl', '_eClass_template_parse_template_Before_addon_tpl');
+
+function _eClass_template_parse_template_Before_addon_tpl($_EVENT, &$ret) {
+
+}
+
+?>
