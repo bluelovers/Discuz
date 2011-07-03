e5c6c37b175e29272b23ec6226dedc7f662a65b1
bluelovers <codelovers@users.sourceforge.net>
2011-07-03 22:22:27 +0800

    replace #|javascript: => javascript:void(0)

--- discuzx/upload/extensions/hooks/hooks_cache.php
+++ discuzx/upload/extensions/hooks/hooks_cache.php
@@ -12,6 +12,10 @@ function _eClass_template_parse_template_Before_addon_tpl($_EVENT, $ret) {
 
 	$data = &$_EVENT['event.data'];
 
+	// replace #|javascript: => javascript:void(0)
+	$data['find'][] = "/\s+href=(\"|\')(?:(?:javascript\:;)|\#+)\\1/is";
+	$data['replace'][] = " href=\\1javascript:void(0);\\1";
+
 	// {js uri}
 	$data['find'][] = "/[\n\r\t]*\{js(?:\:|\s+)(.+?)\}[\n\r\t]*/ies";
 	$data['replace'][] = "\$this->stripvtags('<script src=\"<? echo \$_G[\'setting\'][\'jspath\']; ?>\\1?<?=VERHASH?>\" type=\"text/javascript\"></script>')";
