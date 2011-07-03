d78f7894eecd53fde2d99232598938731945e0c5
bluelovers <codelovers@users.sourceforge.net>
2011-07-03 22:14:41 +0800

    update hooks/hooks_cache.php

--- discuzx/upload/extensions/hooks/hooks_cache.php
+++ discuzx/upload/extensions/hooks/hooks_cache.php
@@ -8,8 +8,12 @@ if (!discuz_core::$plugin_support['Scorpio_Event']) return false;
 
 Scorpio_Hook::add('Class_template::parse_template:Before_addon_tpl', '_eClass_template_parse_template_Before_addon_tpl');
 
-function _eClass_template_parse_template_Before_addon_tpl($_EVENT, &$ret) {
+function _eClass_template_parse_template_Before_addon_tpl($_EVENT, $ret) {
 
+	$_EVENT['event.data']['find'][] = "/[\n\r\t]*\{js(?:\:|\s+)(.+?)\}[\n\r\t]*/ies";
+	$_EVENT['event.data']['replace'][] = "\$this->stripvtags('<script src=\"<? echo \$_G[\'setting\'][\'jspath\']; ?>\\1?<?=VERHASH?>\" type=\"text/javascript\"></script>')";
+
+	return Scorpio_Hook::RET_SUCCESS;
 }
 
 ?>
