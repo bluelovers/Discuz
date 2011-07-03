87862223a32e9deb4986cbc21fa77d64771158b0
bluelovers <codelovers@users.sourceforge.net>
2011-07-03 22:17:07 +0800

    update tpl js

--- discuzx/upload/extensions/hooks/hooks_cache.php
+++ discuzx/upload/extensions/hooks/hooks_cache.php
@@ -10,6 +10,7 @@ Scorpio_Hook::add('Class_template::parse_template:Before_addon_tpl', '_eClass_te
 
 function _eClass_template_parse_template_Before_addon_tpl($_EVENT, $ret) {
 
+	// {js uri}
 	$_EVENT['event.data']['find'][] = "/[\n\r\t]*\{js(?:\:|\s+)(.+?)\}[\n\r\t]*/ies";
 	$_EVENT['event.data']['replace'][] = "\$this->stripvtags('<script src=\"<? echo \$_G[\'setting\'][\'jspath\']; ?>\\1?<?=VERHASH?>\" type=\"text/javascript\"></script>')";
 
