a2e5f7be1a6b708b4a6ed2b739c7e8978ef3c74e
bluelovers <codelovers@users.sourceforge.net>
2011-07-03 22:25:37 +0800

    {變量:default 默認值}

--- discuzx/upload/extensions/hooks/hooks_cache.php
+++ discuzx/upload/extensions/hooks/hooks_cache.php
@@ -16,6 +16,10 @@ function _eClass_template_parse_template_Before_addon_tpl($_EVENT, $ret) {
 	$data['find'][] = "/\s+href=(\"|\')(?:(?:javascript\:;)|\#+)\\1/is";
 	$data['replace'][] = " href=\\1javascript:void(0);\\1";
 
+	// {變量:default 默認值}
+	$data['find'][] = "/[\n\r\t]*\{\<\?\=$var_regexp\?\>\:default\s+([^\{\}].*?)\}[\n\r\t]*/ies";
+	$data['replace'][] = "\$this->addquote('<?= ((!isset(\\1) || empty(\\1)) ? \\5 : \\1) ?>')";
+
 	// {js uri}
 	$data['find'][] = "/[\n\r\t]*\{js(?:\:|\s+)(.+?)\}[\n\r\t]*/ies";
 	$data['replace'][] = "\$this->stripvtags('<script src=\"<? echo \$_G[\'setting\'][\'jspath\']; ?>\\1?<?=VERHASH?>\" type=\"text/javascript\"></script>')";
