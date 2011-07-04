87a5939e01e8cc081240397d9ed5f5cfec361807
bluelovers <codelovers@users.sourceforge.net>
2011-07-04 13:45:44 +0800

    update test/template

---
+++ discuzx/upload/extensions/test/template.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ *
+ */
+
+//define('APPTYPEID', 100);
+//define('CURSCRIPT', 'misc');
+
+require './source/class/class_core.php';
+
+$discuz = & discuz_core::instance();
+
+include template('subblock/test/template');
+
+?>
