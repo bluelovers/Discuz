82ba5626e769e0a91bf83e7242e8f026c33bf8ea
bluelovers <codelovers@users.sourceforge.net>
2011-07-04 20:07:49 +0800

    改用 imagecopymerge_alpha
    如果使用 GD 原生的 imagecopymerge 則透明的周邊會產生白色雜點
    單靠 imagecopymerge_alpha 仍無法成功透明必須依賴上方的代碼

--- discuzx/upload/source/class/class_image.php
+++ discuzx/upload/source/class/class_image.php
@@ -238,6 +238,49 @@ class image {
 		return $im;
 	}
 
+	function imagecopymerge_alpha($dst_im, $src_im, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h, $pct, $trans = NULL)
+		{
+		$dst_w = imagesx($dst_im);
+		$dst_h = imagesy($dst_im);
+
+		// bounds checking
+		$src_x = max($src_x, 0);
+		$src_y = max($src_y, 0);
+		$dst_x = max($dst_x, 0);
+		$dst_y = max($dst_y, 0);
+		if ($dst_x + $src_w > $dst_w)
+			$src_w = $dst_w - $dst_x;
+		if ($dst_y + $src_h > $dst_h)
+			$src_h = $dst_h - $dst_y;
+
+		for($x_offset = 0; $x_offset < $src_w; $x_offset++)
+			for($y_offset = 0; $y_offset < $src_h; $y_offset++)
+			{
+				// get source & dest color
+				$srccolor = imagecolorsforindex($src_im, imagecolorat($src_im, $src_x + $x_offset, $src_y + $y_offset));
+				$dstcolor = imagecolorsforindex($dst_im, imagecolorat($dst_im, $dst_x + $x_offset, $dst_y + $y_offset));
+
+				// apply transparency
+				if (is_null($trans) || ($srccolor !== $trans))
+				{
+					$src_a = $srccolor['alpha'] * $pct / 100;
+					// blend
+					$src_a = 127 - $src_a;
+					$dst_a = 127 - $dstcolor['alpha'];
+					$dst_r = ($srccolor['red'] * $src_a + $dstcolor['red'] * $dst_a * (127 - $src_a) / 127) / 127;
+					$dst_g = ($srccolor['green'] * $src_a + $dstcolor['green'] * $dst_a * (127 - $src_a) / 127) / 127;
+					$dst_b = ($srccolor['blue'] * $src_a + $dstcolor['blue'] * $dst_a * (127 - $src_a) / 127) / 127;
+					$dst_a = 127 - ($src_a + $dst_a * (127 - $src_a) / 127);
+					$color = imagecolorallocatealpha($dst_im, $dst_r, $dst_g, $dst_b, $dst_a);
+					// paint
+					if (!imagesetpixel($dst_im, $dst_x + $x_offset, $dst_y + $y_offset, $color))
+						return false;
+					imagecolordeallocate($dst_im, $color);
+				}
+			}
+		return true;
+	}
+
 	function Thumb_GD() {
 		global $_G;
 
@@ -263,6 +306,8 @@ class image {
 //		imagecolortransparent($copy_photo, imagecolorallocatealpha($copy_photo, 0, 0, 0, 127));
 		// bluelovers
 
+		@$imagefunc($copy_photo, $this->target.'.1.1.png');
+
 		imagecopy($copy_photo, $attach_photo ,0, 0, 0, 0, $this->imginfo['width'], $this->imginfo['height']);
 		$attach_photo = $copy_photo;
 
@@ -286,9 +331,26 @@ class image {
 				if(!($this->imginfo['width'] <= $this->param['thumbwidth'] || $this->imginfo['height'] <= $this->param['thumbheight'])) {
 					list($startx, $starty, $cutw, $cuth) = $this->sizevalue(1);
 					$dst_photo = imagecreatetruecolor($cutw, $cuth);
-					imagecopymerge($dst_photo, $attach_photo, 0, 0, $startx, $starty, $cutw, $cuth, 100);
 
-					// temp debug code - 此階段裁切為正方形(已失去透明)
+					// bluelovers
+					$bg = imagecolortransparent($dst_photo, imagecolorallocatealpha($dst_photo, 255, 255, 255, 127));
+				    imagefill($dst_photo, 0, 0, $bg);
+				    imagefilledrectangle($dst_photo, 0, 0, $cutw, $cuth, $bg);
+
+					@$imagefunc($dst_photo, $this->target.'.2.1.png');
+					// bluelovers
+
+					/**
+					 * 改用 imagecopymerge_alpha
+					 * @see http://www.php.net/manual/en/function.imagecopymerge.php#102844
+					 *
+					 * 如果使用 GD 原生的 imagecopymerge 則透明的周邊會產生白色雜點
+					 * 單靠 imagecopymerge_alpha 仍無法成功透明必須依賴上方的代碼
+					 * @see http://www.php.net/manual/en/function.imagecopymerge.php#102496
+					 **/
+					$this->imagecopymerge_alpha($dst_photo, $attach_photo, 0, 0, $startx, $starty, $cutw, $cuth, 100);
+
+					// temp debug code - 此階段裁切為正方形(成功透明)
 					@$imagefunc($dst_photo, $this->target.'.2.png');
 
 					$thumb_photo = imagecreatetruecolor($this->param['thumbwidth'], $this->param['thumbheight']);
