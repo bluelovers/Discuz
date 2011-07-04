f03ae61d90711d459f43a26cb37875ef7f2a138a
bluelovers <codelovers@users.sourceforge.net>
2011-07-04 15:34:16 +0800

    part.g.1

--- discuzx/upload/source/module/forum/forum_group.php
+++ discuzx/upload/source/module/forum/forum_group.php
@@ -410,7 +410,9 @@ if($action == 'index') {
 
 			$iconsql = '';
 			$deletebanner = $_G['gp_deletebanner'];
+			if ($_FILES['iconnew']) {
 			$iconnew = upload_icon_banner($_G['forum'], $_FILES['iconnew'], 'icon');
+			}
 			$bannernew = upload_icon_banner($_G['forum'], $_FILES['bannernew'], 'banner');
 			if($iconnew) {
 				$iconsql .= ", icon='$iconnew'";
