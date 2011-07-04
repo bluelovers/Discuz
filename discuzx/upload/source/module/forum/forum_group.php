487e9b824ea38501c5beac6a0bf7f3002ee3554d
bluelovers <codelovers@users.sourceforge.net>
2011-07-04 18:39:50 +0800

    預防(非預期/不合法)的刪除，增加可刪除 icon

--- discuzx/upload/source/module/forum/forum_group.php
+++ discuzx/upload/source/module/forum/forum_group.php
@@ -462,7 +462,8 @@ if($action == 'index') {
 			}
 			// bluelovers
 
-			if($iconnew) {
+			// 增加 deleteicon
+			if($iconnew && empty($deleteicon)) {
 				$iconsql .= ", icon='$iconnew'";
 				$group_recommend = unserialize($_G['setting']['group_recommend']);
 				if($group_recommend[$_G['fid']]) {
@@ -471,12 +472,25 @@ if($action == 'index') {
 					include libfile('function/cache');
 					updatecache('setting');
 				}
+			} elseif($deleteicon && discuz_core::$plugin_support['scofile']) {
+				$iconsql .= ", icon=''";
+				// 安全的刪除
+				$_path = scofile::file($_G['forum']['icon']);
+				$_root = scofile::path($_G['setting']['attachurl'].'group/');
+				if (strpos($_path, $_root) === 0) {
+					@scofile::unlink($_path);
+				}
 			}
 			if($bannernew && empty($deletebanner)) {
 				$iconsql .= ", banner='$bannernew'";
-			} elseif($deletebanner) {
+			} elseif($deletebanner && discuz_core::$plugin_support['scofile']) {
 				$iconsql .= ", banner=''";
-				@unlink($_G['forum']['banner']);
+				// 安全的刪除
+				$_path = scofile::file($_G['forum']['banner']);
+				$_root = scofile::path($_G['setting']['attachurl'].'group/');
+				if (strpos($_path, $_root) === 0) {
+					@scofile::unlink($_path);
+				}
 			}
 			$_G['gp_descriptionnew'] = censor(trim($_G['gp_descriptionnew']));
 			$censormod = censormod($_G['gp_descriptionnew']);
