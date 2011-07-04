676be8e223e6642cd6dfb6d8ba0b05a2928ed549
bluelovers <codelovers@users.sourceforge.net>
2011-07-04 13:50:06 +0800

    bugfix

--- discuzx/upload/extensions/test/template.php
+++ discuzx/upload/extensions/test/template.php
@@ -7,7 +7,7 @@
 //define('APPTYPEID', 100);
 //define('CURSCRIPT', 'misc');
 
-require './source/class/class_core.php';
+require './../../source/class/class_core.php';
 
 $discuz = & discuz_core::instance();
 
