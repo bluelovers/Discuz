e3609bb81234eeda91beed16250832037e3978f8
bluelovers <codelovers@users.sourceforge.net>
2011-07-04 21:03:31 +0800

    update style

--- discuzx/upload/extensions/test/image.php
+++ discuzx/upload/extensions/test/image.php
@@ -31,7 +31,7 @@ $i++;
 $img = new image;
 $img->Thumb($_img_r, $_img_d.'.test.'.$i.'.png', 48, 48, 'fixnone');
 
-echo '<style>body { background-image: url(image/bg-0062.gif); }</style>';
+echo '<style>body { background-image: url(image/bg-0062.gif); } img { border: 1px solid blue; }</style>';
 
 for ($k=0; $k<=$i; $k++) {
 	echo '<img src="'.$_img_d.'.test.'.$k.'.png'.'"><br>';
