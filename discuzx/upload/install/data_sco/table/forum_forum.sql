de438b13011881c4be20e3de206042c3fc76ece0
bluelovers <codelovers@users.sourceforge.net>
2011-07-07 03:06:25 +0800

    將原來的 smallint(6) 改為 TINYINT( 3 ) 減少不必要的浪費資料庫

--- discuzx/upload/install/data_sco/table/forum_forum.sql
+++ discuzx/upload/install/data_sco/table/forum_forum.sql
@@ -1,3 +1,6 @@
 
 # 使 pre_forum_forum.recommend 轉為 TEXT 來儲存 fid 字串陣列
 ALTER TABLE `pre_forum_forum` CHANGE `recommend` `recommend` TEXT NOT NULL ;
+
+#將原來的 smallint(6) 改為 TINYINT( 3 ) 減少不必要的浪費資料庫
+ALTER TABLE `pre_forum_forum` CHANGE `displayorder` `displayorder` TINYINT( 1 ) NOT NULL DEFAULT '0';
