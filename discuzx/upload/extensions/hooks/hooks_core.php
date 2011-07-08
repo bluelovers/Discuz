b7d102581ef5d08619fcb9eb8291f73660f0638e
bluelovers <codelovers@users.sourceforge.net>
2011-07-09 00:27:12 +0800

    bugfix - part.3

--- discuzx/upload/extensions/hooks/hooks_core.php
+++ discuzx/upload/extensions/hooks/hooks_core.php
@@ -164,9 +164,10 @@ function _eFunc_cachedata_Before_get_syscache($_EVENT, $conf) {
 				// bugfix 修正造成插件語言包無法緩存的問題
 				&& substr($k, 0, 6) != 'plugin'
 			) {
-				$_del_cache[$k] = true;
 				DB::query("DELETE FROM ".DB::table('common_syscache')." WHERE cname = '$k' LIMIT 1");
 			}
+
+			$_del_cache[$k] = true;
 		}
 	}
 }
