22286b43764c1cdb1c326cf3214376cd0947d8d0
bluelovers <codelovers@users.sourceforge.net>
2011-07-09 00:54:58 +0800

    顯示語言包的 index 名稱 - part.2

--- discuzx/upload/source/function/function_admincp.php
+++ discuzx/upload/source/function/function_admincp.php
@@ -654,7 +654,7 @@ function showsetting($setname, $varname, $value, $type = 'radio', $disabled = ''
 					$_s_add = "<span class=\"lightfont\"".($varname && !is_array($varname[0]) ? " title=\"$varname[0]\"" : "")."> ( $varary[0] )</span>";
 					// bluelovers
 
-					$s .= '<li'.($radiocheck[$varary[0]] ? ' class="checked"' : '').$addstyle.'><input class="radio" type="radio"'.($varnameid ? ' id="_v'.md5($varary[0]).'_'.$varnameid.'"' : '').' name="'.$varname[0].'" value="'.$varary[0].'"'.$radiocheck[$varary[0]].$check['disabled'].$onclick.'>&nbsp;'.$varary[1].'</li>';
+					$s .= '<li'.($radiocheck[$varary[0]] ? ' class="checked"' : '').$addstyle.'><input class="radio" type="radio"'.($varnameid ? ' id="_v'.md5($varary[0]).'_'.$varnameid.'"' : '').' name="'.$varname[0].'" value="'.$varary[0].'"'.$radiocheck[$varary[0]].$check['disabled'].$onclick.'>&nbsp;'.$varary[1].$_s_add.'</li>';
 				}
 			}
 			$s .= '</ul>';
