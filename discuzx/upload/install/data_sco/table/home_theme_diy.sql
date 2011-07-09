4f60c4562e7c867f339519cbdef2081a470b5e22
bluelovers <codelovers@users.sourceforge.net>
2011-07-10 02:42:09 +0800

    自定義風格

---
+++ discuzx/upload/install/data_sco/table/home_theme_diy.sql
@@ -0,0 +1,10 @@
+
+DROP TABLE IF EXISTS `pre_home_theme_diy`;
+CREATE TABLE `pre_home_theme_diy` (
+`theme_id` SMALLINT( 6 ) UNSIGNED NOT NULL AUTO_INCREMENT ,
+`theme_fup_id` SMALLINT( 6 ) UNSIGNED NOT NULL DEFAULT '0',
+`theme_name` VARCHAR( 30 ) NOT NULL DEFAULT '',
+`theme_css` TEXT NOT NULL ,
+PRIMARY KEY ( `theme_id` ) ,
+INDEX ( `theme_fup_id` )
+) ENGINE = MYISAM COMMENT = '自定義風格';
