5dbf93a138ff838eef01e1a9638d95e0e218046d
bluelovers <codelovers@users.sourceforge.net>
2011-07-10 23:19:39 +0800

    簡化重複代碼

--- discuzx/upload/source/function/function_cache.php
+++ discuzx/upload/source/function/function_cache.php
@@ -26,19 +26,13 @@ function updatecache($cachename = '') {
 		$cachedirhandle = dir($cachedir);
 		while($entry = $cachedirhandle->read()) {
 			if(!in_array($entry, array('.', '..')) && preg_match("/^cache\_([\_\w]+)\.php$/", $entry, $entryr) && $entryr[1] != 'setting' && substr($entry, -4) == '.php' && is_file($cachedir.'/'.$entry)) {
-				@include_once libfile('cache/'.$entryr[1], 'function');
-				// bluelovers
-				if (function_exists('build_cache_'.$entryr[1])) {
-				// bluelovers
-					call_user_func('build_cache_'.$entryr[1]);
-				// bluelovers
-				} else {
-					$lostcaches[] = $entryr[1];
-				}
-				// bluelovers
+				// 簡化重複代碼
+				$updatelist[] = $entryr[1];
 			}
 		}
-	} else {
+	}
+
+	if ($updatelist) {
 		foreach($updatelist as $entry) {
 			@include_once libfile('cache/'.$entry, 'function');
 			// bluelovers
