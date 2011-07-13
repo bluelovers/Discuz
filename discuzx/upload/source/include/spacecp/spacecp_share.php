c0f9a39f3d93128bc644bdf8cde23f278c247e98
bluelovers <codelovers@users.sourceforge.net>
2011-07-14 00:47:41 +0800

    part.e.4

--- discuzx/upload/source/include/spacecp/spacecp_share.php
+++ discuzx/upload/source/include/spacecp/spacecp_share.php
@@ -538,10 +538,10 @@ if($_GET['op'] == 'delete') {
 		if (discuz_core::$plugin_support['Scorpio_Event']) {
 			Scorpio_Event::instance('Dz_module_'.basename(__FILE__, '.php').':Before_notification')
 				->run(array(array(
-				'arr' => &$arr,
-				'setarr' => &$setarr,
-				'sid' => $sid,
-				'feedid' => $_feedid,
+					'arr' => &$arr,
+					'setarr' => &$setarr,
+					'sid' => $sid,
+					'feedid' => $_feedid,
 			)));
 		}
 		// bluelovers
