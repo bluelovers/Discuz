103ddf420d2ae0a42e8978d71bde010902746396
bluelovers <codelovers@users.sourceforge.net>
2011-07-14 01:16:54 +0800

    Scorpio_Hook::add('Func_mkfeed:Before', '_eFunc_mkfeed_Before');

--- discuzx/upload/extensions/hooks/hooks_share.php
+++ discuzx/upload/extensions/hooks/hooks_share.php
@@ -15,4 +15,26 @@ function _eFunc_mkshare_Before($share = array()) {
 	}
 }
 
+Scorpio_Hook::add('Func_mkfeed:Before', '_eFunc_mkfeed_Before');
+
+function _eFunc_mkfeed_Before(&$feed) {
+
+	$_lang_template = empty($feed['lang_template']) ? array() : unserialize($feed['lang_template']);
+	if (is_array($_lang_template)) {
+		foreach ($_lang_template as $_k_ => $_v_) {
+			$feed[$_k_] = is_array($_v_) ? call_user_func_array('lang', $_v_) : lang('feed', $_v_);
+
+			if ($feed['icon'] == 'share' && !strexists($feed[$_k_], '{actor}')) {
+				$feed[$_k_] = '{actor} '.$feed[$_k_];
+			}
+		}
+	}
+
+	if ($feed['icon'] == 'share') {
+		$feed['title_data'] = $feed['body_data'];
+	}
+
+//	dexit($feed);
+}
+
 ?>
