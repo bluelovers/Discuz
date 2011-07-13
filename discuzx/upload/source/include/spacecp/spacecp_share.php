552f902170e0544469b4cca83aa7a7dfddd8c1f6
bluelovers <codelovers@users.sourceforge.net>
2011-07-14 00:48:32 +0800

    Event: Dz_module_spacecp_share:Before_notification

--- discuzx/upload/source/include/spacecp/spacecp_share.php
+++ discuzx/upload/source/include/spacecp/spacecp_share.php
@@ -535,6 +535,7 @@ if($_GET['op'] == 'delete') {
 		}
 
 		// bluelovers
+		// Event: Dz_module_spacecp_share:Before_notification
 		if (discuz_core::$plugin_support['Scorpio_Event']) {
 			Scorpio_Event::instance('Dz_module_'.basename(__FILE__, '.php').':Before_notification')
 				->run(array(array(
