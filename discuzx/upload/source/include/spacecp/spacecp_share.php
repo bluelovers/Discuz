c8b216af65bd37798e6b00ee6f3fd6d5faef476a
bluelovers <codelovers@users.sourceforge.net>
2011-07-14 00:51:54 +0800

    part.g.1

--- discuzx/upload/source/include/spacecp/spacecp_share.php
+++ discuzx/upload/source/include/spacecp/spacecp_share.php
@@ -496,6 +496,15 @@ if($_GET['op'] == 'delete') {
 
 		$arr['body_data'] = serialize($arr['body_data']);
 
+		// bluelovers
+		if (sclass_exists('Scorpio_Hook')) {
+			Scorpio_Hook::execute('Dz_module_'.basename(__FILE__, '.php').':Before_share', array(array(
+				'arr' => &$arr,
+				'setarr' => &$setarr,
+			)));
+		}
+		// bluelovers
+
 		$setarr = daddslashes($arr);
 
 		// bluelovers
