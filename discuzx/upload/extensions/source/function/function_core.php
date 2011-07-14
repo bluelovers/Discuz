c46bf9e679493e0fde99fcfb1de0f8b995588aaa
bluelovers <codelovers@users.sourceforge.net>
2011-07-14 20:49:27 +0800

    array_push_array - Push one or more array onto the end of array

--- discuzx/upload/extensions/source/function/function_core.php
+++ discuzx/upload/extensions/source/function/function_core.php
@@ -4,4 +4,25 @@
  * @author bluelovers
  **/
 
+/**
+ * Push one or more array onto the end of array
+ *
+ * @param &$array
+ * @param $args
+ **/
+function array_push_array(&$array, $args) {
+	$args = func_get_args();
+	array_shift($args);
+
+	if (count($args)) {
+	    foreach ($args as $v) {
+	    	if (!empty($v)) {
+				foreach ($v as $r) {
+					array_push($array, $r);
+				}
+			}
+		}
+	}
+}
+
 ?>
