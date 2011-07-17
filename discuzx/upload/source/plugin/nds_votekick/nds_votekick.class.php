38725a58f2d1d38b706899e1c38c4fba97299a23
bluelovers <codelovers@users.sourceforge.net>
2011-07-17 23:03:07 +0800

    add desc

--- discuzx/upload/source/plugin/nds_votekick/nds_votekick.class.php
+++ discuzx/upload/source/plugin/nds_votekick/nds_votekick.class.php
@@ -14,6 +14,7 @@ class plugin_nds_votekick {
 	function plugin_nds_votekick() {
 		global $_G;
 		$this->lvotes = $_G['cache']['plugin']['nds_votekick']['lvotes'];
+		// 帖子可投票时间范围（天数）
 		$this->vtexpired = $_G['cache']['plugin']['nds_votekick']['vtexpired'];
 		$this->voteforums = unserialize($_G['cache']['plugin']['nds_votekick']['voteforums']);
 		//$this->votegroups = unserialize($_G['cache']['plugin']['nds_votekick']['votegroups']);
@@ -24,6 +25,7 @@ class plugin_nds_votekick_forum extends plugin_nds_votekick {
 		global $_G;
 		if (!$_G['uid']) return;
 		if (in_array($_G['fid'], $this->voteforums)) return;
+		// 判斷帖子可投票时间范围（天数）
 		if ($this->vtexpired > 0 && ($_G['timestamp'] - $_G['forum_thread']['dateline']) >
 			$this->vtexpired * 24 * 60 * 60) return;
 		$votemax = $this->lvotes;
