440a95837091a2f265d19a2b648909c33ce209de
bluelovers <codelovers@users.sourceforge.net>
2011-07-20 14:49:58 +0800

    ok

--- discuzx/upload/source/plugin/sco_avatar/sco_avatar.class.php
+++ discuzx/upload/source/plugin/sco_avatar/sco_avatar.class.php
@@ -1,9 +1,16 @@
 <?php
 
-class plugin_sco_avatar extends _sco_dx_plugin {
+include_once './sco_dx_plugin.class.php';
+
+class plugin_sco_avatar {
 	function plugin_sco_avatar() {
+		/*
 		$this->_init($this->_get_identifier(__METHOD__));
+		*/
 	}
+}
+
+class plugin_sco_avatar_home extends plugin_sco_avatar{
 }
 
 ?>
