bf9f0d1638846a030536ee289fa7fdcfdd79392a
bluelovers <codelovers@users.sourceforge.net>
2011-07-20 14:48:20 +0800

    .

--- discuzx/upload/source/plugin/sco_avatar/sco_dx_plugin.class.php
+++ discuzx/upload/source/plugin/sco_avatar/sco_dx_plugin.class.php
@@ -6,12 +6,6 @@ class _sco_dx_plugin {
 	var $attr = array();
 
 	function _init($identifier) {
-		global $_G;
-
-		if(!isset($_G['cache']['plugin'])) {
-			loadcache('plugin');
-		}
-
 		$this->identifier = $identifier;
 		$this->attr['identifier'] = &$this->identifier;
 
