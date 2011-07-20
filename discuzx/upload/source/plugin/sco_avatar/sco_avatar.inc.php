836cb088e06068dbaf39aa256194a23c85b96d11
bluelovers <codelovers@users.sourceforge.net>
2011-07-21 02:40:57 +0800

    $avatar_path = getgpc('avatar_path', null,'default', 1);

--- discuzx/upload/source/plugin/sco_avatar/sco_avatar.inc.php
+++ discuzx/upload/source/plugin/sco_avatar/sco_avatar.inc.php
@@ -5,7 +5,9 @@ include_once 'sco_avatar.class.php';
 // 建立 class plugin_$identifier
 $plugin_self = _sco_dx_plugin::_instance($identifier, $module);
 
-$_loop_avatar = $plugin_self->_loop_glob($plugin_self->attr['directory'].'image/avatar/default', '*');
+$avatar_path = getgpc('avatar_path', null,'default', 1);
+
+$_loop_avatar = $plugin_self->_loop_glob($plugin_self->attr['directory'].'image/avatar/'.$avatar_path, '*');
 
 include template('common/header');
 
