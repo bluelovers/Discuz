c2b6c4648968dd10fd64824d7c9079423db96e02
bluelovers <codelovers@users.sourceforge.net>
2011-07-21 00:51:05 +0800

    .

--- discuzx/upload/source/plugin/sco_avatar/sco_avatar.inc.php
+++ discuzx/upload/source/plugin/sco_avatar/sco_avatar.inc.php
@@ -3,6 +3,8 @@
 include_once 'sco_avatar.class.php';
 
 // 建立 class plugin_$identifier
-$plugin_self = _sco_dx_plugin::_instance($identifier);
+$plugin_self = _sco_dx_plugin::_instance($identifier, $module);
+
+
 
 ?>
