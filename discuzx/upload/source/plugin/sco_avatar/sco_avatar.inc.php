d36afadd4537a85424cc3b800191bee057f662ea
bluelovers <codelovers@users.sourceforge.net>
2011-07-21 01:56:18 +0800

    .

--- discuzx/upload/source/plugin/sco_avatar/sco_avatar.inc.php
+++ discuzx/upload/source/plugin/sco_avatar/sco_avatar.inc.php
@@ -5,13 +5,21 @@ include_once 'sco_avatar.class.php';
 // 建立 class plugin_$identifier
 $plugin_self = _sco_dx_plugin::_instance($identifier, $module);
 
+$_loop_avatar = $plugin_self->_loop_glob($plugin_self->attr['directory'].'image/avatar/default', '*');
+
+include template('common/header');
+
+include $plugin_self->_template('avatar');
+
 echo '<pre>';
 
 var_dump(array(
 	$identifier, $module,
-	$plugin_self
+	$plugin_self,
 ));
 
-dexit();
+echo '</pre>';
+
+include template('common/footer');
 
 ?>
