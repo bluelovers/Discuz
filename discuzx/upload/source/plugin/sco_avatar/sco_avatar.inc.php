d64ac683d12e7c9f9674bbe7ad55b2e6b2108640
bluelovers <codelovers@users.sourceforge.net>
2011-07-22 11:31:24 +0800

    .

--- discuzx/upload/source/plugin/sco_avatar/sco_avatar.inc.php
+++ discuzx/upload/source/plugin/sco_avatar/sco_avatar.inc.php
@@ -5,36 +5,66 @@ include_once 'sco_avatar.class.php';
 // 建立 class plugin_$identifier
 $plugin_self = _sco_dx_plugin::_instance($identifier, $module);
 
-// 取得目前瀏覽的 avatar 目錄
-$avatar_path = getgpc('avatar_path', null,'default', 1);
-
-$_loop_avatar = $plugin_self->_loop_glob($plugin_self->attr['directory'].'image/avatar/'.$avatar_path, '*');
-
-include template('common/header');
+$avatar_base_path = $plugin_self->attr['directory'].'image/avatar/';
 
 // 取得所有 avatar 目錄
-$_avatar_types = array();
-$d = dir($plugin_self->attr['directory'].'image/avatar/');
-while (false !== ($entry = $d->read())) {
-	if ($entry == '.' || $entry == '..') continue;
+$avatar_types = array();
 
-	array_push($_avatar_types, $entry);
+$avatar_types['default'] = 'default';
+
+$d = dir($avatar_base_path);
+while (false !== ($entry = $d->read())) {
+	if ($entry == '.' || $entry == '..' || $entry == 'default') continue;
+
+	$avatar_types[$entry] = $entry;
 }
 
-var_dump($_avatar_types);
+// 取得目前瀏覽的 avatar 目錄
+$avatar_view_path = getgpc('avatar_view_path', null,'default', 1);
+$avatar_view_path = in_array($avatar_view_path, $avatar_types) ? $avatar_view_path : 'default';
 
-include $plugin_self->_template('avatar');
+$imgexts = array('jpg', 'jpeg', 'gif', 'png', 'bmp');
+
+$avatar_pics = array();
+$d = dir($avatar_base_path.$avatar_view_path);
+while (false !== ($entry = $d->read())) {
+	if ($entry == '.' || $entry == '..'
+		|| !in_array(fileext($entry), $imgexts)
+	) continue;
+
+	$avatar_pics[$entry] = $avatar_base_path.$avatar_view_path.'/'.$entry;
+}
+
+include $plugin_self->_template('spacecp_avatar');
 
 echo '<pre>';
 
-$member_uc = $plugin_self
-	->_uc_init()
-	->_uc_call('sc', 'set_user_fields', array(
-		'uid' => $_G['uid'],
-		'fields'=> array(
-			'avatar' => $_G['siteurl'].array_shift($_loop_avatar),
-		),
+$a_file = getgpc('a_file');
+
+var_dump(array(
+	$a_file,
 ));
+
+if (empty($a_file) || empty($avatar_pics[$a_file])) {
+	unset($a_file);
+} else {
+	$a_file = $avatar_pics[$a_file];
+}
+
+var_dump(array(
+	$a_file,
+));
+
+if (!empty($a_file)) {
+	$member_uc = $plugin_self
+		->_uc_init()
+		->_uc_call('sc', 'set_user_fields', array(
+			'uid' => $_G['uid'],
+			'fields'=> array(
+				'avatar' => $_G['siteurl'].$a_file,
+			),
+	));
+}
 
 $member_uc2 = $plugin_self
 	->_uc_init()
@@ -47,7 +77,7 @@ $member_uc2 = $plugin_self
 
 var_dump(array(
 	$member_uc,
-	$member_uc2
+	$member_uc2,
 ));
 
 var_dump(array(
@@ -55,7 +85,5 @@ var_dump(array(
 ));
 
 echo '</pre>';
-
-include template('common/footer');
 
 ?>
