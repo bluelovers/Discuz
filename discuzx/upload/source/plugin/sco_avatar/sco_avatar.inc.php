2442024949864fe52aff232710a624d3bb93c710
bluelovers <codelovers@users.sourceforge.net>
2011-07-22 13:20:38 +0800

    .

--- discuzx/upload/source/plugin/sco_avatar/sco_avatar.inc.php
+++ discuzx/upload/source/plugin/sco_avatar/sco_avatar.inc.php
@@ -9,9 +9,9 @@ $avatar_base_path = $plugin_self->attr['directory'].'image/avatar/';
 
 $plugin_self->_my_avatar_types_list();
 
-$plugin_self->_my_avatar_view_path(getgpc('avatar_view_path'));
-
-$plugin_self->_my_avatar_pics();
+$plugin_self->_my_avatar_pics(
+	$plugin_self->_my_avatar_view_path(getgpc('avatar_view_path'))
+);
 
 extract($plugin_self->attr['global']);
 
@@ -21,41 +21,21 @@ echo '<pre>';
 
 $a_file = getgpc('a_file');
 
-var_dump(array(
-	$a_file,
-));
-
 if (empty($a_file) || empty($avatar_pics[$a_file])) {
 	unset($a_file);
 } else {
 	$a_file = $avatar_pics[$a_file];
 }
 
-var_dump(array(
-	$a_file,
-));
-
 if (!empty($a_file)) {
-	$member_uc = $plugin_self
-		->_uc_init()
-		->_uc_call('sc', 'set_user_fields', array(
-			'uid' => $_G['uid'],
-			'fields'=> array(
-				'avatar' => $_G['siteurl'].$a_file,
-			),
-	));
+	$member_uc = $plugin_self->_my_avatar_user_save($_G['uid'], $_G['siteurl'].$a_file);
 }
 
-$member_uc2 = $plugin_self
-	->_uc_init()
-	->_uc_call('sc', 'get_user_fields', array(
-		'uid' => $_G['uid'],
-		'fields'=> array(
-			'avatar',
-		),
-));
+$member_uc2 = $plugin_self->_my_avatar_user_get($_G['uid']);
 
 var_dump(array(
+	$a_file,
+
 	$member_uc,
 	$member_uc2,
 ));
