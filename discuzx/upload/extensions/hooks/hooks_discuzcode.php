d6616f0f59dd612fe8433346809d9d45511fce09
bluelovers <codelovers@users.sourceforge.net>
2011-07-23 17:05:40 +0800

    .

--- discuzx/upload/extensions/hooks/hooks_discuzcode.php
+++ discuzx/upload/extensions/hooks/hooks_discuzcode.php
@@ -3,37 +3,6 @@
 if (!discuz_core::$plugin_support['Scorpio_Event']) return false;
 
 class _bbcode_ {
-	function codedisp($code, $brush = 'plain') {
-		global $_G;
-		$_G['forum_discuzcode']['pcodecount']++;
-		/*
-		$code = dhtmlspecialchars(str_replace('\\"', '"', preg_replace("/^[\n\r]*(.+?)[\n\r]*$/is", "\\1", $code)));
-		$code = str_replace("\n", "<li>", $code);
-		*/
-		$code = str_replace(
-			array(
-				"\t",
-				"\n",
-				'\\"',
-				'\\"'
-			), array(
-				'[tab][/tab]',
-				'[br][/br]',
-				'"',
-				'"'
-			)
-			, $code);
-
-		$brush = empty($brush) ? 'plain' : $brush;
-
-		discuz_core::$plugin_support['SyntaxHighlighter'][$brush] = $brush;
-
-		$code = '<pre name="theCode" class="brush: '.$brush.';">'.dhtmlspecialchars($code).'</pre>';
-
-		$_G['forum_discuzcode']['codehtml'][$_G['forum_discuzcode']['pcodecount']] = $code;
-		$_G['forum_discuzcode']['codecount']++;
-		return "[\tDISCUZ_CODE_".$_G['forum_discuzcode']['pcodecount']."\t]";
-	}
 }
 
 ?>
