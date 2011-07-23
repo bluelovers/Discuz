4607e2fb23b74de438ca391c9e3fe7f3e11fb68a
bluelovers <codelovers@users.sourceforge.net>
2011-07-23 16:58:46 +0800

    update code

--- discuzx/upload/source/function/function_discuzcode.php
+++ discuzx/upload/source/function/function_discuzcode.php
@@ -46,8 +46,23 @@ function creditshide($creditsrequire, $message, $pid) {
 function codedisp($code, $brush = 'plain') {
 	global $_G;
 	$_G['forum_discuzcode']['pcodecount']++;
+	/*
 	$code = dhtmlspecialchars(str_replace('\\"', '"', preg_replace("/^[\n\r]*(.+?)[\n\r]*$/is", "\\1", $code)));
 	$code = str_replace("\n", "<li>", $code);
+	*/
+	$code = str_replace(
+		array(
+			"\t",
+			"\n",
+			'\\"',
+			'\\"'
+		), array(
+			'[tab][/tab]',
+			'[br][/br]',
+			'"',
+			'"'
+		)
+		, $code);
 	$_G['forum_discuzcode']['codehtml'][$_G['forum_discuzcode']['pcodecount']] = tpl_codedisp($code);
 	$_G['forum_discuzcode']['codecount']++;
 	return "[\tDISCUZ_CODE_".$_G['forum_discuzcode']['pcodecount']."\t]";
