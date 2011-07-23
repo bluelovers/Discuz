708e515fb6dae460f6e8e68958561f64b02cf90e
bluelovers <codelovers@users.sourceforge.net>
2011-07-23 16:06:28 +0800

    Event: Func_discuzcode:Before_bbcodes - part.2

--- discuzx/upload/source/function/function_discuzcode.php
+++ discuzx/upload/source/function/function_discuzcode.php
@@ -229,24 +229,28 @@ function discuzcode($message, $smileyoff = 0, $bbcodeoff = 0, $htmlon = 0, $allo
 		}
 
 		// bluelovers
-		Scorpio_Hook::execute('Func_'.__FUNCTION__.':Before_bbcodes', array(array(
-				'message'			=> &$message
-				, 'smileyoff'		=> &$smileyoff
-				, 'bbcodeoff'		=> &$bbcodeoff
-				, 'htmlon'			=> &$htmlon
-				, 'allowsmilies'	=> &$allowsmilies
-				, 'allowbbcode'		=> &$allowbbcode
-				, 'allowimgcode'	=> &$allowimgcode
-				, 'allowhtml'		=> &$allowhtml
-				, 'jammer'			=> &$jammer
-				, 'parsetype'		=> &$parsetype
-				, 'authorid'		=> &$authorid
-				, 'allowmediacode'	=> &$allowmediacode
-				, 'pid'				=> &$pid
-				, 'msglower'		=> &$msglower
+		if (discuz_core::$plugin_support['Scorpio_Event']) {
+			// Event: Func_discuzcode:Before_bbcodes
+			Scorpio_Event::instance('Func_'.__FUNCTION__.':Before_bbcodes')
+				->run(array(array(
+					'message'			=> &$message
+					, 'smileyoff'		=> &$smileyoff
+					, 'bbcodeoff'		=> &$bbcodeoff
+					, 'htmlon'			=> &$htmlon
+					, 'allowsmilies'	=> &$allowsmilies
+					, 'allowbbcode'		=> &$allowbbcode
+					, 'allowimgcode'	=> &$allowimgcode
+					, 'allowhtml'		=> &$allowhtml
+					, 'jammer'			=> &$jammer
+					, 'parsetype'		=> &$parsetype
+					, 'authorid'		=> &$authorid
+					, 'allowmediacode'	=> &$allowmediacode
+					, 'pid'				=> &$pid
+					, 'msglower'		=> &$msglower
 
-				, 'authorreplyexist' => &$authorreplyexist
-		)));
+					, 'authorreplyexist' => &$authorreplyexist
+			)));
+		}
 		// bluelovers
 
 		$message = str_replace(array(
