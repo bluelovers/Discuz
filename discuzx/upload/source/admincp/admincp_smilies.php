75fcdb8036dcfa93dcce9486829258a7c85869fb
bluelovers <codelovers@users.sourceforge.net>
2011-07-23 13:08:12 +0800

    [desc] 修正依照自然順序時的起始數字以 10 為基準的錯誤

--- discuzx/upload/source/admincp/admincp_smilies.php
+++ discuzx/upload/source/admincp/admincp_smilies.php
@@ -202,6 +202,7 @@ if(!$operation) {
 				var prefix = trim($(pre + 'prefix').value);
 				var suffix = trim($(pre + 'suffix').value);
 				var page = parseInt('$page');
+				// 修正依照自然順序時的起始數字以 10 為基準的錯誤
 				var middle = $(pre + 'middle').value == 1 ? $(pre + 'url_' + i).value.substr(0,$(pre + 'url_' + i).value.lastIndexOf('.')) : ($(pre + 'middle').value == 2 ? i + (page > 0 ? page - 1 : 0) * 10 : $(pre + 'code_'+ i).attributes['smileyid'].nodeValue);
 				if(!prefix || prefix == '$lang[smilies_prefix]' || !suffix || suffix == '$lang[smilies_suffix]') {
 					alert('$lang[smilies_prefix_tips]');
