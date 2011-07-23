5013016153cfb27d75bfb0549ca7c0fdcf120770
bluelovers <codelovers@users.sourceforge.net>
2011-07-23 11:38:08 +0800

    .

--- discuzx/upload/static/js/forum.js
+++ discuzx/upload/static/js/forum.js
@@ -367,7 +367,9 @@ function loadData(quiet, formobj) {
 }
 
 // 控制檢查版塊的主題變化的時間間隔
-var checkForumcount = 0, checkForumtimeout = 30000, checkForumnew_handle;
+var checkForumcount = 0
+	, checkForumtimeout = 30000
+	, checkForumnew_handle;
 function checkForumnew(fid, lasttime) {
 	// ajax 顯示板塊是否有新的主題變化
 	var timeout = checkForumtimeout;
