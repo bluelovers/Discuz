478da57aab6d28cbb1322faf3cb76c4b677fb10d
bluelovers <codelovers@users.sourceforge.net>
2011-07-23 17:00:42 +0800

    .

--- discuzx/upload/source/function/function_discuzcode.php
+++ discuzx/upload/source/function/function_discuzcode.php
@@ -72,7 +72,10 @@ function codedisp($code, $brush = 'plain') {
 	$code = '<pre name="theCode" class="brush: '.$brush.';">'.dhtmlspecialchars($code).'</pre>';
 	// bluelovers
 
+	/*
 	$_G['forum_discuzcode']['codehtml'][$_G['forum_discuzcode']['pcodecount']] = tpl_codedisp($code);
+	*/
+	$_G['forum_discuzcode']['codehtml'][$_G['forum_discuzcode']['pcodecount']] = $code;
 	$_G['forum_discuzcode']['codecount']++;
 	return "[\tDISCUZ_CODE_".$_G['forum_discuzcode']['pcodecount']."\t]";
 }
