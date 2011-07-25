3c5b58692a0265d54ddfa4f21c0d3e00eeae4fb5
bluelovers <codelovers@users.sourceforge.net>
2011-07-25 16:22:00 +0800

    fix

--- discuzx/utility/oldprg/discuz/tag.php
+++ discuzx/utility/oldprg/discuz/tag.php
@@ -4,13 +4,15 @@ error_reporting(E_ERROR);
 ob_start();
 header("HTTP/1.1 301 Moved Permanently");
 
-$url = 'misc.php?mod=tag&';
+$url = 'misc.php?mod=tag';
 
 // 避免被覆寫 mod
 unset($_GET['mod']);
 
-// 直接取用 $_GET
-$url .= http_build_query((array)$_GET);
+if (!empty($_GET)) {
+	// 直接取用 $_GET
+	$url .= '&'.http_build_query((array)$_GET);
+}
 
 header("location: $url");
 
