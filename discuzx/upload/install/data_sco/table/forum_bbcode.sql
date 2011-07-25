1c17fd7b7e3f16cd00916270489d026e496a17cb
bluelovers <codelovers@users.sourceforge.net>
2011-07-25 12:55:52 +0800

    使 icon 可以接受更長的值

--- discuzx/upload/install/data_sco/table/forum_bbcode.sql
+++ discuzx/upload/install/data_sco/table/forum_bbcode.sql
@@ -1,3 +1,6 @@
 
 # 可設定參數類型 資料陣列以 \t 分隔
-ALTER TABLE `pre_forum_bbcode` ADD `pattern` VARCHAR( 30 ) NOT NULL COMMENT '可設定參數類型 資料陣列以 \\t 分隔'
+ALTER TABLE `pre_forum_bbcode` ADD `pattern` VARCHAR( 30 ) NOT NULL COMMENT '可設定參數類型 資料陣列以 \\t 分隔';
+
+# 使 icon 可以接受更長的值
+ALTER TABLE `pre_forum_bbcode` CHANGE `icon` `icon` TEXT NOT NULL ;
