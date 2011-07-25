52f36eb4e4e62303d29caf2673f69edb31efa056
bluelovers <codelovers@users.sourceforge.net>
2011-07-25 09:03:21 +0800

    Scorpio_Hook::add('Func_build_cache_bbcodes:Before_define3', '_eFunc_build_cache_bbcodes_Before_define3');

--- discuzx/upload/extensions/hooks/cache/cache_bbcodes.php
+++ discuzx/upload/extensions/hooks/cache/cache_bbcodes.php
@@ -6,4 +6,12 @@ if(!defined('IN_DISCUZ')) {
 
 if (!discuz_core::$plugin_support['Scorpio_Event']) return false;
 
+Scorpio_Hook::add('Func_build_cache_bbcodes:Before_define3', '_eFunc_build_cache_bbcodes_Before_define3');
+
+function _eFunc_build_cache_bbcodes_Before_define3(&$conf) {
+	extract($conf, EXTR_REFS);
+
+	$bbcode['replacement'] = str_replace('{bbtag}', $bbcode['tag'], $bbcode['replacement']);
+}
+
 ?>
