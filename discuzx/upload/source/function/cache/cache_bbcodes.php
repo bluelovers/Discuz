a4c868117687df9443fc2a386d80f1014123101c
bluelovers <codelovers@users.sourceforge.net>
2011-07-25 08:44:03 +0800

    Event: Func_build_cache_bbcodes:Before_define3 - part.2

--- discuzx/upload/source/function/cache/cache_bbcodes.php
+++ discuzx/upload/source/function/cache/cache_bbcodes.php
@@ -146,13 +146,16 @@ function build_cache_bbcodes() {
 		$switchstop = 0;
 
 		//TODO:Event: Func_build_cache_bbcodes:Before_define3
-		Scorpio_Hook::execute('Func_' . __FUNCTION__ . ':Before_define3', array(array('regexp' => &$regexp,
+		if (discuz_core::$plugin_support['Scorpio_Event']) {
+			Scorpio_Event::instance('Func_' . __FUNCTION__ . ':Before_define3')
+				->run(array(array('regexp' => &$regexp,
 					'bbcode' => &$bbcode,
 					'search' => &$search,
 					'switchstop' => &$switchstop,
 					'replace' => &$replace,
 					'data' => &$data,
-					)));
+			)));
+		}
 
 		if (!$switchstop) {
 		// bluelovers
