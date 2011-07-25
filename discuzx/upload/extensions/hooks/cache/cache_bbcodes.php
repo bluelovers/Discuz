8eea2d7e5d71f0812f8c560e53b886a1f6f58261
bluelovers <codelovers@users.sourceforge.net>
2011-07-25 12:22:47 +0800

    bugfix

--- discuzx/upload/extensions/hooks/cache/cache_bbcodes.php
+++ discuzx/upload/extensions/hooks/cache/cache_bbcodes.php
@@ -35,7 +35,7 @@ function _eFunc_build_cache_bbcodes_Before_init_regexp($_EVENT, $_conf) {
 Scorpio_Hook::add('Func_build_cache_bbcodes:Before_define1', '_eFunc_build_cache_bbcodes_Before_define1');
 
 function _eFunc_build_cache_bbcodes_Before_define1($_EVENT, $_conf) {
-	extract($conf, EXTR_REFS);
+	extract($_conf, EXTR_REFS);
 
 	//TODO:使用 discuz_core::$_cache_data['bbcodes']['regexp_base']
 	if (!empty(discuz_core::$_cache_data['bbcodes']['regexp_ex'])) {
@@ -75,7 +75,7 @@ function _eFunc_build_cache_bbcodes_Before_define1($_EVENT, $_conf) {
 Scorpio_Hook::add('Func_build_cache_bbcodes:Before_define3', '_eFunc_build_cache_bbcodes_Before_define3');
 
 function _eFunc_build_cache_bbcodes_Before_define3($_EVENT, $_conf) {
-	extract($conf, EXTR_REFS);
+	extract($_conf, EXTR_REFS);
 
 	$bbcode['replacement'] = str_replace('{bbtag}', $bbcode['tag'], $bbcode['replacement']);
 }
