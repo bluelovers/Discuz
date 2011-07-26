68b91391e5ca845ee7e850842af1807cc4d9a129
bluelovers <codelovers@users.sourceforge.net>
2011-07-26 13:49:35 +0800

    Scorpio_Hook::add('Func_writetocsscache:Before_minify', '_eFunc_writetocsscache_Before_minify');

--- discuzx/upload/extensions/hooks/cache/cache_styles.php
+++ discuzx/upload/extensions/hooks/cache/cache_styles.php
@@ -10,4 +10,11 @@ if(!defined('IN_DISCUZ')) {
 
 if (!discuz_core::$plugin_support['Scorpio_Event']) return false;
 
+Scorpio_Hook::add('Func_writetocsscache:Before_minify', '_eFunc_writetocsscache_Before_minify');
+
+function _eFunc_writetocsscache_Before_minify($_EVENT, $conf) {
+	extract($conf, EXTR_REFS);
+
+}
+
 ?>
