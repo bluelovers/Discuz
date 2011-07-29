e8d61bb53ed67a9b6970a582b619122d62cd6133
bluelovers <codelovers@users.sourceforge.net>
2011-07-29 23:23:12 +0800

    增加排序 , available DESC, name ASC - part.2

--- discuzx/upload/source/admincp/admincp_smilies.php
+++ discuzx/upload/source/admincp/admincp_smilies.php
@@ -49,6 +49,7 @@ if(!$operation) {
 
 		$smtypes = 0;
 		$dirfilter = array();
+		// 增加排序 , available DESC, name ASC
 		$query = DB::query("SELECT * FROM ".DB::table('forum_imagetype')." WHERE type='smiley' ORDER BY displayorder, available DESC, name ASC");
 		while($type = DB::fetch($query)) {
 			$squery = DB::query("SELECT COUNT(*) FROM ".DB::table('common_smiley')." WHERE typeid='$type[typeid]'");
