3c7607d88fadd8e4acfeb2c936e45218ca624d61
bluelovers <codelovers@users.sourceforge.net>
2011-07-29 10:17:29 +0800

    Scorpio_Hook::add('Class_discuz_core::_init:After', '_eClass_discuz_core__init_After');

--- discuzx/upload/extensions/hooks/template/goodnight/hooks_core.php
+++ discuzx/upload/extensions/hooks/template/goodnight/hooks_core.php
@@ -11,4 +11,16 @@ if(!defined('IN_DISCUZ')) {
 
 if (!discuz_core::$plugin_support['Scorpio_Event']) return false;
 
+Scorpio_Hook::add('Class_discuz_core::_init:After', '_eClass_discuz_core__init_After');
+
+/**
+ * Event:
+ * 		Class_discuz_core::_init:After
+ */
+function _eClass_discuz_core__init_After($_EVENT, $discuz) {
+
+
+
+}
+
 ?>
