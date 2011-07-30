11d9a651292be120c085a730fc6d5bb7ec7bdc52
bluelovers <codelovers@users.sourceforge.net>
2011-07-30 16:18:05 +0800

    function is_url($url)

--- discuzx/utility/convert/include/bbcode.class.php
+++ discuzx/utility/convert/include/bbcode.class.php
@@ -149,10 +149,22 @@ class bbcode {
 			'media',
 
 			'youtube',
-			'audio',
+
 			'flash',
+			'flv',
 
 			'wmv',
+			'rmvb',
+			'yamflv',
+			'wretch',
+			'gv',
+
+			'audio',
+			'wma',
+			'ra',
+			'rm',
+			'mp3',
+			'yammp3',
 		);
 
 		if ($_regex) {
@@ -196,6 +208,65 @@ class bbcode {
 
 		return '['.$tag.$r.']'.$value.'[/'.$tag.']';
 	}
+
+	/**
+	 * Validate a URL.
+	 *
+	 * @param   string   URL
+	 * @return  boolean
+	 */
+	function is_url($url) {
+		// Based on http://www.apps.ietf.org/rfc/rfc1738.html#sec-5
+		if ( ! preg_match(
+			'~^
+
+			# scheme
+			[-a-z0-9+.]++://
+
+			# username:password (optional)
+			(?:
+				    [-a-z0-9$_.+!*\'(),;?&=%]++   # username
+				(?::[-a-z0-9$_.+!*\'(),;?&=%]++)? # password (optional)
+				@
+			)?
+
+			(?:
+				# ip address
+				\d{1,3}+(?:\.\d{1,3}+){3}+
+
+				| # or
+
+				# hostname (captured)
+				(
+					     (?!-)[-a-z0-9]{1,63}+(?<!-)
+					(?:\.(?!-)[-a-z0-9]{1,63}+(?<!-)){0,126}+
+				)
+			)
+
+			# port (optional)
+			(?::\d{1,5}+)?
+
+			# path (optional)
+			(?:/.*)?
+
+			$~iDx', $url, $matches))
+			return FALSE;
+
+		// We matched an IP address
+		if ( ! isset($matches[1]))
+			return TRUE;
+
+		// Check maximum length of the whole hostname
+		// http://en.wikipedia.org/wiki/Domain_name#cite_note-0
+		if (strlen($matches[1]) > 253)
+			return FALSE;
+
+		// An extra check for the top level domain
+		// It must start with a letter
+		$tld = ltrim(substr($matches[1], (int) strrpos($matches[1], '.')), '.');
+		return ctype_alpha($tld[0]);
+	}
+
 	// bluelovers
 }
 
