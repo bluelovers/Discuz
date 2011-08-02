bcc0a4cee89327a84b7d4cb2bbd2d069afbc59b3
bluelovers <codelovers@users.sourceforge.net>
2011-08-02 15:41:23 +0800

    task_reward

--- discuzx/upload/source/language/task/lang_template.php
+++ discuzx/upload/source/language/task/lang_template.php
@@ -2,7 +2,11 @@
 
 $lang = array
 (
+	'task_reward_condition' => '領取此任務獎賞所需條件',
 
+	'task_complete_time_reward_start' => '從完成任務後開始，',
+	'task_complete_time_reward_limit' => '計時 $taskvars[complete][time_reward][value] 小時後，',
+	'task_complete_time_reward_get' => '即可領取。',
 );
 
 ?>
