a699f63b951bef5ae022da2f7654c5f9e2c7f088
bluelovers <codelovers@users.sourceforge.net>
2011-08-06 01:38:47 +0800

    set instance = $this

--- discuzx/upload/source/plugin/sco_toolbar/sco_toolbar.class.php
+++ discuzx/upload/source/plugin/sco_toolbar/sco_toolbar.class.php
@@ -9,6 +9,9 @@ include_once libfile('class/sco_dx_plugin', 'source', 'extensions/');
 class plugin_sco_toolbar extends _sco_dx_plugin {
 	function plugin_sco_toolbar() {
 		$this->_init($this->_get_identifier(__METHOD__));
+
+		// set instance = $this
+		$this->_this(&$this);
 	}
 }
 
