c561bd3c3f873d5bfb69cbfff613a3625d9b8366
bluelovers <codelovers@users.sourceforge.net>
2011-08-05 19:51:32 +0800

    0718更新內容: 1. 修復設置默認"展開/收縮"工具欄無效的問題 2. 修復在工具欄點擊並"登陸"跳轉的問題 3. "在線好友"功能增加了"打招呼"功能, 用戶可在"在線好友"好友列表裡點擊右邊的"打招呼"圖標,這時就會彈出"打招呼"彈框. 更新方法: 只需下載新文件後解壓上傳覆蓋原文件即可, 並不涉及到設置的變更.

--- discuzx/upload/source/plugin/lnp_taskbar_dzx/lnp_taskbar.class.php
+++ discuzx/upload/source/plugin/lnp_taskbar_dzx/lnp_taskbar.class.php
@@ -20,7 +20,7 @@ class plugin_lnp_taskbar_dzx {
 		$staticurl = STATICURL;
 		$lnpdata = $_G['cache']['plugin']['lnp_taskbar_dzx'];
 		$toplist_h = $lnpdata['toplist_h'] ? 'height:'.$lnpdata['toplist_h'].'px;' : '';
-		if(getcookie('lnp_topen') == 1 || (getcookie('lnp_topen') != -1 && $lnddata['openbar'])) {
+		if(getcookie('lnp_topen') == 1 || (getcookie('lnp_topen') != -1 && $lnpdata['open_bar'])) {
 			$disopen = '';
 			$disclose = ' style="display:none;" ';
 		} else {
@@ -103,14 +103,14 @@ class plugin_lnp_taskbar_dzx {
 		space_merge($user, 'field_home');
 			
 		if($_G['uid'] > 0 && $user['feedfriend']) {
-			$olnum = DB::result(DB::query("SELECT COUNT(*) FROM ".DB::table('common_session')." WHERE uid IN ({$user['feedfriend']})"), 0);
+			$olnum = DB::result(DB::query("SELECT COUNT(*) FROM ".DB::table('common_session')." WHERE uid IN ({$user['feedfriend']}) AND invisible='0'"), 0);
 			$olnum = $olnum ? $olnum : 0;
 		}
 		if($olnum && $user['feedfriend']) {
 			$query = DB::query("SELECT uid,username,invisible FROM ".DB::table("common_session")." WHERE uid IN ({$user['feedfriend']}) AND invisible='0' ORDER BY lastactivity DESC LIMIT 20");
 			while($value = DB::fetch($query)) {
 				$liststr .= <<<EOT
-				<li><img class="lnp_olicon" title="在线" src="{$staticurl}image/common/ol.gif" /><a title="和 {$value['username']} 聊天" href="home.php?mod=space&amp;uid={$value['uid']}" onclick="showWindow('showMsgBox{$value['uid']}', 'home.php?mod=spacecp&ac=pm&op=showmsg&handlekey=showmsg_{$value['uid']}&touid={$value['uid']}&pmid=0&daterange=2', 'get', 0)" ><img alt="{$value['username']}" src="{$staticurl}image/common/user_online.gif">{$value['username']}</a></li>
+				<li><img id="lnp_poke_u{$value['uid']}" onclick="showWindow(this.id, 'home.php?mod=spacecp&amp;ac=poke&amp;op=send&uid={$value['uid']}', 'get', 0);" class="lnp_olicon" title="点击图片和{$value['username']}打招呼!" src="{$staticurl}image/feed/poke.gif" /><a title="点击这里和 {$value['username']} 聊天" href="home.php?mod=space&amp;uid={$value['uid']}" onclick="showWindow('showMsgBox{$value['uid']}', 'home.php?mod=spacecp&amp;ac=pm&amp;op=showmsg&amp;handlekey=showmsg_{$value['uid']}&amp;touid={$value['uid']}&pmid=0&daterange=2', 'get', 0)" ><img alt="{$value['username']}" src="{$staticurl}image/common/user_online.gif">{$value['username']}</a></li>
 EOT;
 			}
 		}
