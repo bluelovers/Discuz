0f0daabe28920b97788c4c7bf4a56f30154f4d5c
bluelovers <codelovers@users.sourceforge.net>
2011-08-06 01:10:25 +0800

    init plugin_sco_toolbar

---
+++ discuzx/upload/source/plugin/sco_toolbar/sco_toolbar.class.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+include_once libfile('class/sco_dx_plugin', 'source', 'extensions/');
+
+class plugin_sco_toolbar extends _sco_dx_plugin {
+
+}
+
+?>
