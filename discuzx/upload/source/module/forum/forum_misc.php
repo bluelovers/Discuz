afc6e79ef383b9c27705cbbf475a5c7fdcb459d9
bluelovers <codelovers@users.sourceforge.net>
2011-08-06 15:22:37 +0800

    修改為主題列表可顯示評分分數 - part.4

--- discuzx/upload/source/module/forum/forum_misc.php
+++ discuzx/upload/source/module/forum/forum_misc.php
@@ -766,7 +766,11 @@ if($_G['gp_action'] == 'votepoll' && submitcheck('pollsubmit', 1)) {
 			}
 			DB::query("UPDATE ".DB::table($posttable)." SET rate=rate+($rate), ratetimes=ratetimes-$ratetimes WHERE pid='$_G[gp_pid]'");
 			if($post['first']) {
+				/*
 				$threadrate = @intval(@($post['rate'] + $rate) / abs($post['rate'] + $rate));
+				*/
+				// 修改為主題列表可顯示評分分數
+				$threadrate = intval($post['rate'] + $rate);
 				DB::query("UPDATE ".DB::table('forum_thread')." SET rate='$threadrate' WHERE tid='$_G[tid]'");
 			}
 
