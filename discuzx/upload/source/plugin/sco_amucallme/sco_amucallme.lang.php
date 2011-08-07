1671497d0b2442ce48347492d408dae96716e1e0
bluelovers <codelovers@users.sourceforge.net>
2011-08-07 21:20:12 +0800

    lang

---
+++ discuzx/upload/source/plugin/sco_amucallme/sco_amucallme.lang.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2011
+ */
+
+$installlang['sco_amucallme'] = array();
+
+$scriptlang['sco_amucallme'] = array(
+	'callme' => '呼我',
+	'sendmsg' => '您好！{username}邀請您參與話題討論（在帖子中提到了您），具體內容(<A HREF="{url}" target="_blank" class="lit">查看更多內容...</A>)：<blockquote>{message}</blockquote>您也可以直接回復他：<A HREF="{reply}" onclick="showWindow(\'reply\', this.href)" class="lit">馬上回復</A>。',
+	'admin2_h1' => '@功能消費設定',
+	'admin2_t2' => '積分類型',
+	'admin2_t3' => '積分數量',
+	'admin2_t4' => '有效用戶組',
+	'admin2_p1' => '<li>這裡您可以對各個用戶組會員使用@功\\能，設置各自的費用：</li><li>比如您設置，新手上路 1金幣 ，表示新手上路會員@一個用戶時需要消耗1金幣，@ N個會員時，需要N個金幣。</li>',
+	'admin2_s1' => '添加',
+	'admin2_s2' => '提交',
+	'admin2_i' => '提交完成!',
+	'sf' => '<span class="xi1">&nbsp;&nbsp;收費標準：{cost}/人*次</span>',
+);
+
+$templatelang['sco_amucallme'] = array(
+	'cm' => '呼我吧',
+	'sb' => '搜索',
+	'ss' => '搜索看看',
+	'lz' => '(樓主)',
+	'u' => '這是我們給您推薦的',
+	'f' => '這是隨機推薦的好友',
+	'g' => '您可以試試@用戶組',
+	'jgshow' => '搜索結果顯示區：',
+	'noshow' => '沒有您搜索到相關會員',
+);
+
+?>
