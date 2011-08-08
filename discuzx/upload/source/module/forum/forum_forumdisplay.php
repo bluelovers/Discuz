26f6bd89634dbc31d8b55abdd90df7190f351a38
bluelovers <codelovers@users.sourceforge.net>
2011-08-08 20:14:41 +0800

    [desc] 'forum_passwd_incorrect' => '抱歉，您輸入的密碼不正確，不能訪問這個版塊',

--- discuzx/upload/source/module/forum/forum_forumdisplay.php
+++ discuzx/upload/source/module/forum/forum_forumdisplay.php
@@ -165,6 +165,7 @@ if($_G['forum']['viewperm'] && !forumperm($_G['forum']['viewperm']) && !$_G['for
 if($_G['forum']['password']) {
 	if($_G['gp_action'] == 'pwverify') {
 		if($_G['gp_pw'] != $_G['forum']['password']) {
+			// 'forum_passwd_incorrect' => '抱歉，您輸入的密碼不正確，不能訪問這個版塊',
 			showmessage('forum_passwd_incorrect', NULL);
 		} else {
 			dsetcookie('fidpw'.$_G['fid'], $_G['gp_pw']);
