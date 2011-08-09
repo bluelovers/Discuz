081589c61f375e042a8e186c9bdf17153df9fe92
bluelovers <codelovers@users.sourceforge.net>
2011-08-09 22:42:29 +0800

    function build_cache_groups_list()

---
+++ discuzx/upload/source/function/cache/cache_groups_list.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2011
+ */
+
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+function build_cache_groups_list() {
+	$data = array();
+
+}
+
+?>
