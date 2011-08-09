ecf644525da0b0604093ae7ef47063955e495378
bluelovers <codelovers@users.sourceforge.net>
2011-08-09 22:35:42 +0800

    [desc] function build_cache_grouptype()

--- discuzx/upload/source/function/cache/cache_grouptype.php
+++ discuzx/upload/source/function/cache/cache_grouptype.php
@@ -11,6 +11,100 @@ if(!defined('IN_DISCUZ')) {
 	exit('Access Denied');
 }
 
+/**
+ * 群組分類
+ *
+ * $data['grouptype'] = array (
+  'first' =>
+  array (
+    208 =>
+    array (
+      'fid' => '208',
+      'fup' => '0',
+      'name' => '空間群組',
+      'forumcolumns' => '0',
+      'membernum' => '0',
+      'groupnum' => 17,
+      'secondlist' =>
+      array (
+        0 => '209',
+        1 => '217',
+        2 => '218',
+        3 => '222',
+        4 => '225',
+        5 => '228',
+        6 => '230',
+      ),
+    ),
+  ),
+  'second' =>
+  array (
+    209 =>
+    array (
+      'fid' => '209',
+      'fup' => '208',
+      'name' => '自由聯盟',
+      'forumcolumns' => '0',
+      'membernum' => '0',
+      'groupnum' => '7',
+    ),
+    217 =>
+    array (
+      'fid' => '217',
+      'fup' => '208',
+      'name' => '地區聯盟',
+      'forumcolumns' => '0',
+      'membernum' => '0',
+      'groupnum' => '0',
+    ),
+    218 =>
+    array (
+      'fid' => '218',
+      'fup' => '208',
+      'name' => '興趣聯盟',
+      'forumcolumns' => '0',
+      'membernum' => '0',
+      'groupnum' => '3',
+    ),
+    222 =>
+    array (
+      'fid' => '222',
+      'fup' => '208',
+      'name' => '站內大雜燴',
+      'forumcolumns' => '0',
+      'membernum' => '0',
+      'groupnum' => '2',
+    ),
+    225 =>
+    array (
+      'fid' => '225',
+      'fup' => '208',
+      'name' => '動漫聯盟',
+      'forumcolumns' => '0',
+      'membernum' => '0',
+      'groupnum' => '2',
+    ),
+    228 =>
+    array (
+      'fid' => '228',
+      'fup' => '208',
+      'name' => '聯盟特區',
+      'forumcolumns' => '0',
+      'membernum' => '0',
+      'groupnum' => '1',
+    ),
+    230 =>
+    array (
+      'fid' => '230',
+      'fup' => '208',
+      'name' => '遊戲聯盟',
+      'forumcolumns' => '0',
+      'membernum' => '0',
+      'groupnum' => '2',
+    ),
+  ),
+);
+ */
 function build_cache_grouptype() {
 	$data = array();
 	$query = DB::query("SELECT f.fid, f.fup, f.name, f.forumcolumns, ff.membernum, ff.groupnum FROM ".DB::table('forum_forum')." f
