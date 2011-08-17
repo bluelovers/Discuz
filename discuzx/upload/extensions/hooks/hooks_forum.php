2c541392144863162b4597752acd88c0d9cc87f4
bluelovers <codelovers@users.sourceforge.net>
2011-08-18 01:08:37 +0800

    Scorpio_Hook::add('Script_forum_post:Before_action_include', '_eScript_forum_post_Before_action_include');

--- discuzx/upload/extensions/hooks/hooks_forum.php
+++ discuzx/upload/extensions/hooks/hooks_forum.php
@@ -11,4 +11,10 @@ if(!defined('IN_DISCUZ')) {
 
 if (!discuz_core::$plugin_support['Scorpio_Event']) return false;
 
+Scorpio_Hook::add('Script_forum_post:Before_action_include', '_eScript_forum_post_Before_action_include');
+
+function _eScript_forum_post_Before_action_include($_EVENT, $_conf) {
+	extract($_conf, EXTR_REFS);
+}
+
 ?>
