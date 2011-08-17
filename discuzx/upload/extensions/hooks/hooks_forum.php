25513b884c102f91d41431bd5d8fb796655b3075
bluelovers <codelovers@users.sourceforge.net>
2011-08-18 01:06:56 +0800

    init extensions/hooks/hooks_forum.php

---
+++ discuzx/upload/extensions/hooks/hooks_forum.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2011
+ */
+
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+if (!discuz_core::$plugin_support['Scorpio_Event']) return false;
+
+?>
