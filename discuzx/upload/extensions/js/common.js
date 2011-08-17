d74ef98daeeea11caa011b09c3bd6ce657cb9bc0
bluelovers <codelovers@users.sourceforge.net>
2011-08-18 05:54:16 +0800

    EXTRAFUNC['hooks']['clearcode'].push(function(str)

--- discuzx/upload/extensions/js/common.js
+++ discuzx/upload/extensions/js/common.js
@@ -709,7 +709,107 @@
 		return message;
 	});
 
+	if (!EXTRAFUNC['hooks']['clearcode']) EXTRAFUNC['hooks']['clearcode'] = new Array();
 	EXTRAFUNC['hooks']['clearcode'].push(function(str) {
+
+		var m1 = str.match(/\[font=([^\]]*)?\]/g);
+		var m2 = array_unique(m1);
+
+		if (count(m2) == 1) {
+			if (m2[0] == '[font=arial,helvetica,sans-serif]') {
+				str = str
+					.replace(/\[font=arial,helvetica,sans-serif\]/ig, '')
+					.replace(/\[\/font\]/, '')
+				;
+			}
+		}
+
+		str = _validate_message(str);
+
 		return str;
 	});
 })();
+
+function count (mixed_var, mode) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +      input by: Waldo Malqui Silva
+    // +   bugfixed by: Soren Hansen
+    // +      input by: merabi
+    // +   improved by: Brett Zamir (http://brett-zamir.me)
+    // +   bugfixed by: Olivier Louvignes (http://mg-crea.com/)
+    // *     example 1: count([[0,0],[0,-4]], 'COUNT_RECURSIVE');
+    // *     returns 1: 6
+    // *     example 2: count({'one' : [1,2,3,4,5]}, 'COUNT_RECURSIVE');
+    // *     returns 2: 6
+    var key, cnt = 0;
+
+    if (mixed_var === null || typeof mixed_var === 'undefined') {
+        return 0;
+    } else if (mixed_var.constructor !== Array && mixed_var.constructor !== Object) {
+        return 1;
+    }
+
+    if (mode === 'COUNT_RECURSIVE') {
+        mode = 1;
+    }
+    if (mode != 1) {
+        mode = 0;
+    }
+
+    for (key in mixed_var) {
+        if (mixed_var.hasOwnProperty(key)) {
+            cnt++;
+            if (mode == 1 && mixed_var[key] && (mixed_var[key].constructor === Array || mixed_var[key].constructor === Object)) {
+                cnt += this.count(mixed_var[key], 1);
+            }
+        }
+    }
+
+    return cnt;
+}
+
+
+function array_unique (inputArr) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Carlos R. L. Rodrigues (http://www.jsfromhell.com)
+    // +      input by: duncan
+    // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   bugfixed by: Nate
+    // +      input by: Brett Zamir (http://brett-zamir.me)
+    // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   improved by: Michael Grier
+    // +   bugfixed by: Brett Zamir (http://brett-zamir.me)
+    // %          note 1: The second argument, sort_flags is not implemented;
+    // %          note 1: also should be sorted (asort?) first according to docs
+    // *     example 1: array_unique(['Kevin','Kevin','van','Zonneveld','Kevin']);
+    // *     returns 1: {0: 'Kevin', 2: 'van', 3: 'Zonneveld'}
+    // *     example 2: array_unique({'a': 'green', 0: 'red', 'b': 'green', 1: 'blue', 2: 'red'});
+    // *     returns 2: {a: 'green', 0: 'red', 1: 'blue'}
+    var key = '',
+        tmp_arr2 = {},
+        val = '';
+
+    var __array_search = function (needle, haystack) {
+        var fkey = '';
+        for (fkey in haystack) {
+            if (haystack.hasOwnProperty(fkey)) {
+                if ((haystack[fkey] + '') === (needle + '')) {
+                    return fkey;
+                }
+            }
+        }
+        return false;
+    };
+
+    for (key in inputArr) {
+        if (inputArr.hasOwnProperty(key)) {
+            val = inputArr[key];
+            if (false === __array_search(val, tmp_arr2)) {
+                tmp_arr2[key] = val;
+            }
+        }
+    }
+
+    return tmp_arr2;
+}
