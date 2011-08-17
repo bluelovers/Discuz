17f95a20c8e452c9c24243811be44e4db68ed167
bluelovers <codelovers@users.sourceforge.net>
2011-08-18 01:21:13 +0800

    .

--- discuzx/upload/extensions/hooks/hooks_forum.php
+++ discuzx/upload/extensions/hooks/hooks_forum.php
@@ -16,21 +16,25 @@ Scorpio_Hook::add('Script_forum_post:Before_action_include', '_eScript_forum_pos
 function _eScript_forum_post_Before_action_include($_EVENT, $_conf) {
 	extract($_conf, EXTR_REFS);
 
-	$subject = preg_replace(array(
-			'/\n|\s{2,}/s',
-			'/^(?:[\s\]\)]+)|(?:[\s\　\[]+)$/is',
-		), array(
-			' ',
-			'',
-		), $subject);
+	if (!empty($subject)) {
+		$subject = preg_replace(array(
+				'/\n|\s{2,}/s',
+				'/^(?:[\s\]\)]+)|(?:[\s\　\[]+)$/is',
+			), array(
+				' ',
+				'',
+			), $subject);
+	}
 
-	$message = preg_replace(array(
-			'/(?:[\ \　]+)(\n|$)/',
-			'/^\n+|\s+$/s',
-		), array(
-			'\\1',
-			'',
-		), $message);
+	if (!empty($subject)) {
+		$message = preg_replace(array(
+				'/(?:[\ \　]+)(\n|$)/',
+				'/^\n+|\s+$/s',
+			), array(
+				'\\1',
+				'',
+			), $message);
+	}
 }
 
 ?>
