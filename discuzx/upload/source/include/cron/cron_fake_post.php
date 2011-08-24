04707b48ab8a2a8f1729c22b1b9d801b81a7c079
bluelovers <codelovers@users.sourceforge.net>
2011-08-24 19:53:54 +0800

    while($post = DB::fetch($query_post))

--- discuzx/upload/source/include/cron/cron_fake_post.php
+++ discuzx/upload/source/include/cron/cron_fake_post.php
@@ -20,11 +20,23 @@ $query = DB::query("SELECT *
 	FROM ".DB::table('forum_thread')."
 	WHERE
 		dateline='0'
-	ORDER BY RAND()
+	ORDER BY
+		RAND()
 	LIMIT 1
 ");
 while($thread = DB::fetch($query)) {
+	$query_post = DB::query("SELECT *
+		FROM ".DB::table('forum_post')."
+		WHERE
+			tid = '{$thread[tid]}'
+		ORDER BY
+			first DESC
+			, dateline ASC
+			, pid ASC
+	");
+	while($post = DB::fetch($query_post)) {
 
+	}
 }
 
 ?>
