8b0dafb9d959a744158ad7d28695079ed2adf7f7
bluelovers <codelovers@users.sourceforge.net>
2011-08-25 00:16:03 +0800

    .

--- discuzx/upload/source/function/function_debug.php
+++ discuzx/upload/source/function/function_debug.php
@@ -160,7 +160,7 @@ elseif(isset($_GET[\''.$mysqlplek.'\'])) {
 		?>';
 	}
 
-	$debug = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head>';
+	$debug .= '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head>';
 	$debug .= "<base href=\"$_G[siteurl]\" />";
 
 	/**
