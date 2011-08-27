0d72ebc04b461e3d7b7b0b94308ce43f87c2e830
bluelovers <codelovers@users.sourceforge.net>
2011-08-27 20:33:00 +0800

    修正升級轉換過程中消失的 message 內容

--- discuzx/utility/convert/source/d7.2_x2.0/posts_fix_empty.php
+++ discuzx/utility/convert/source/d7.2_x2.0/posts_fix_empty.php
@@ -5,6 +5,46 @@
  * @copyright 2011
  */
 
+$curprg = basename(__FILE__);
 
+$table_source = $db_source->tablepre . 'posts';
+$table_target = $db_target->tablepre . 'forum_post';
+
+$limit = 50;
+$start = intval(getgpc('start'));
+$nextid = 0;
+
+$query = $db_source->query("SELECT *
+	FROM
+		$table_target
+	WHERE
+		pid>'$start'
+		AND message = ''
+	ORDER BY
+		pid ASC
+	LIMIT $limit
+");
+while($row = $db_target->fetch_array($query)) {
+	$nextid = $row['pid'];
+
+	$query = $db_source->query("SELECT *
+		FROM
+			$table_source
+		WHERE
+			pid = '$nextid'
+		LIMIT 1
+	");
+	if ($row_old = $db_source->fetch_array($query)) {
+
+		$data = implode_field_value(array(
+			'message' => daddslashes($row_old['message'], 1),
+		), ',', db_table_fields($db_target, $table_target));
+		$db_target->query("UPDATE $table_target SET $data WHERE pid = '$nextid'");
+	}
+}
+
+if($nextid) {
+	showmessage("繼續轉換數據表 ".$table_target." pid > $nextid", "index.php?a=$action&source=$source&prg=$curprg&start=$nextid");
+}
 
 ?>
