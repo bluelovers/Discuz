676457b247f1b4cbbd960c902d079f75b02be9b0
bluelovers <codelovers@users.sourceforge.net>
2011-09-01 02:21:58 +0800

    function submitcheck($var, $allowget = 0, $seccodecheck = 0, $secqaacheck = 0)

--- discuzx/upload/source/plugin/sco_cpanel/sco_cpanel.class.php
+++ discuzx/upload/source/plugin/sco_cpanel/sco_cpanel.class.php
@@ -13,6 +13,10 @@ include_once libfile('class/sco_dx_plugin', 'source', 'extensions/');
 
 class plugin_sco_cpanel extends _sco_dx_plugin {
 
+	function submitcheck($var, $allowget = 0, $seccodecheck = 0, $secqaacheck = 0) {
+		return submitcheck($var, $allowget, $seccodecheck, $secqaacheck);
+	}
+
 }
 
 ?>
