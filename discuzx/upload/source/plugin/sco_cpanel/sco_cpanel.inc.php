a8b56d741c4be0a76cd396126d4a74fa0896be9b
bluelovers <codelovers@users.sourceforge.net>
2011-09-01 02:24:21 +0800

    .

--- discuzx/upload/source/plugin/sco_cpanel/sco_cpanel.inc.php
+++ discuzx/upload/source/plugin/sco_cpanel/sco_cpanel.inc.php
@@ -57,25 +57,25 @@ class plugin_sco_cpanel_threadsorts extends plugin_sco_cpanel {
 
 		} else {
 
-		$threadtypes = '';
+			$threadtypes = '';
 
-		$query = DB::query("SELECT * FROM ".DB::table('forum_typeoption')." WHERE classid='0' ORDER BY displayorder");
-		while($option = DB::fetch($query)) {
-			$threadtypes .= showtablerow('', array('class="td25"', 'class="td28"', '', 'class="td25"'), array(
-				"<input class=\"checkbox\" type=\"checkbox\" name=\"delete[]\" value=\"$option[optionid]\">",
-				"<input type=\"text\" class=\"txt\" size=\"2\" name=\"displayordernew[$option[optionid]]\" value=\"$option[displayorder]\">",
-				"<input type=\"text\" class=\"txt\" size=\"15\" name=\"namenew[$option[optionid]]\" value=\"".dhtmlspecialchars($option['title'])."\">",
-				"<a href=\"".ADMINSCRIPT."?action=threadtypes&operation=typeoption&classid=$option[optionid]\" class=\"act nowrap\">$lang[detail]</a>"
-			), TRUE);
-		}
+			$query = DB::query("SELECT * FROM ".DB::table('forum_typeoption')." WHERE classid='0' ORDER BY displayorder");
+			while($option = DB::fetch($query)) {
+				$threadtypes .= showtablerow('', array('class="td25"', 'class="td28"', '', 'class="td25"'), array(
+					"<input class=\"checkbox\" type=\"checkbox\" name=\"delete[]\" value=\"$option[optionid]\">",
+					"<input type=\"text\" class=\"txt\" size=\"2\" name=\"displayordernew[$option[optionid]]\" value=\"$option[displayorder]\">",
+					"<input type=\"text\" class=\"txt\" size=\"15\" name=\"namenew[$option[optionid]]\" value=\"".dhtmlspecialchars($option['title'])."\">",
+					"<a href=\"".ADMINSCRIPT."?action=threadtypes&operation=typeoption&classid=$option[optionid]\" class=\"act nowrap\">$lang[detail]</a>"
+				), TRUE);
+			}
 
-		showformheader("plugins&operation=config&do=".$this->attr['profile']['pluginid']."&identifier=".$this->identifier."&pmod=".$this->attr['global']['module']['name']."&");
-		showtableheader('threadtype_infotypes');
-		showsubtitle(array('', 'display_order', 'name', ''));
-		echo $threadtypes;
-		showsubmit('typesubmit', 'submit', 'del');
-		showtablefooter();
-		showformfooter();
+			showformheader("plugins&operation=config&do=".$this->attr['profile']['pluginid']."&identifier=".$this->identifier."&pmod=".$this->attr['global']['module']['name']."&");
+			showtableheader('threadtype_infotypes');
+			showsubtitle(array('', 'display_order', 'name', ''));
+			echo $threadtypes;
+			showsubmit('typesubmit', 'submit', 'del');
+			showtablefooter();
+			showformfooter();
 
 		}
 	}
