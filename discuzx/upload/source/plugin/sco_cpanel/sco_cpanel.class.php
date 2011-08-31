018945d3ad9c4feae45119378a6401de1a484167
bluelovers <codelovers@users.sourceforge.net>
2011-09-01 00:07:41 +0800

    init plugin/sco_cpanel/sco_cpanel.class.php

---
+++ discuzx/upload/source/plugin/sco_cpanel/sco_cpanel.class.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2011
+ */
+
+if (!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+include_once libfile('class/sco_dx_plugin', 'source', 'extensions/');
+
+class plugin_sco_cpanel extends _sco_dx_plugin {
+
+}
+
+?>
