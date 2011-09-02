6d5fbd43e79f7db504d2ebbcc4ade2eb73d6c109
bluelovers <codelovers@users.sourceforge.net>
2011-09-02 22:59:01 +0800

    'orderby_rand' => (bool)$_POST['orderby_rand'],

--- discuzx/upload/source/include/portalcp/portalcp_block.php
+++ discuzx/upload/source/include/portalcp/portalcp_block.php
@@ -122,6 +122,11 @@ if($op == 'block') {
 			'hidedisplay' => $_POST['hidedisplay'] ? '1' : '0',
 			'dateline' => TIMESTAMP,
 			'isblank' => $_POST['isblank']
+
+			// bluelovers
+			,
+			'orderby_rand' => (bool)$_POST['orderby_rand'],
+			// bluelovers
 		);
 
 		$picdata = array();
