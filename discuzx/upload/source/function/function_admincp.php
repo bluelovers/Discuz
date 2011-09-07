718b235f6afc9e56aa2612a730a958b4c7ff5938
bluelovers <codelovers@users.sourceforge.net>
2011-09-08 05:33:04 +0800

    jQuery(window).triggerHandler('js_ajaxget');

--- discuzx/upload/source/function/function_admincp.php
+++ discuzx/upload/source/function/function_admincp.php
@@ -368,6 +368,13 @@ jQuery(function(){
 		.find(':checkbox, :radio').each(_func)
 	;
 
+	(function(old_evalscript) {
+		evalscript = function(s) {
+			old_evalscript(s);
+			jQuery(window).triggerHandler('js_ajaxget');
+		};
+	})(evalscript);
+
 });
 </script>
 EOM;
