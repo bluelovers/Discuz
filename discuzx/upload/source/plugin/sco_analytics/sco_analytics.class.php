2ed78453336213de8280fa05119404680da28fd2
bluelovers <codelovers@users.sourceforge.net>
2011-09-09 17:32:15 +0800

    function googleAnalyticsGetImageUrl()

--- discuzx/upload/source/plugin/sco_analytics/sco_analytics.class.php
+++ discuzx/upload/source/plugin/sco_analytics/sco_analytics.class.php
@@ -17,6 +17,26 @@ class plugin_sco_analytics extends _sco_dx_plugin {
 
 class mobileplugin_sco_analytics extends plugin_sco_analytics {
 
+	function googleAnalyticsGetImageUrl() {
+		global $GA_ACCOUNT, $GA_PIXEL;
+		$url = "";
+		$url .= $GA_PIXEL . "?";
+		$url .= "utmac=" . $GA_ACCOUNT;
+		$url .= "&utmn=" . rand(0, 0x7fffffff);
+		$referer = $_SERVER["HTTP_REFERER"];
+		$query = $_SERVER["QUERY_STRING"];
+		$path = $_SERVER["REQUEST_URI"];
+		if (empty($referer)) {
+			$referer = "-";
+		}
+		$url .= "&utmr=" . urlencode($referer);
+		if (!empty($path)) {
+			$url .= "&utmp=" . urlencode($path);
+		}
+		$url .= "&guid=ON";
+		return str_replace("&", "&amp;", $url);
+	}
+
 }
 
 ?>
