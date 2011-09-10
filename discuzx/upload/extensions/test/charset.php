66c4aa7600c885e3572458fca006b2bb9609872c
bluelovers <codelovers@users.sourceforge.net>
2011-09-10 09:55:06 +0800

    .

---
+++ discuzx/upload/extensions/test/charset.php
@@ -0,0 +1,346 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2011
+ */
+
+$title = <<<EOM
+123456789zxcvbnm,./\"\';lkjhgfdsaqwertyuiop[]=-?0987654321`!@#$%^&*()_+}{":><
+見知らぬ少女に叩き起こされ、見知らぬ部屋で目を覚ました主人公・星央彼方 （ほしお かなた）。
+少女に急かされるように学園へと連れて行かれるが、歩く道も学園の名前も覚えが無い。
+しかしやがて、昨夜彼女に自己紹介を受けたことを思い出す。
+樓主收集的夠全的，對這一快確實不清楚，一半寫正則什麼的驗證中文總是習慣
+性的使用\u4e00-\u9FA5
+「私の名前は三多野綾莉 （みたの あやり）。仕事はサンタクロース！」
+
+首を傾げたくなるような自己紹介だったが、思い出せないよりはずっといい。
+見知らぬ学園に着いた彼方は、自分がここに来た経緯と事実を自覚し、ひとり呟くことになる。
+本当に、あの家から逃げて来たんだな、と。
+
+期間内にご予約していただきましたお客様に抽選で100名様に、原画家・神藤みけこ氏の描き下ろしカラーイラストが印刷された色紙を神藤みけこ氏直筆サイン入りでプレゼントいたします !!
+応募方法は公式Webサイトをご覧ください。
+
+キャンペーン期間：2009年3月30日までㄅㄆㄇㄈ
+一二三
+
+ぁぅぇぉゃゅゎょっぃﾇヌﾓモュユ㈹７８９４５６１２３０．，＝＾＋－＊／％＞＜）（｛｝「」
+
+樓主收集的夠全的，對這一快確實不清楚，一半寫正則什麼的驗證中文總是習慣
+
+U+3105	ㄅ	e3 84 85	BOPOMOFO LETTER B
+U+3106	ㄆ	e3 84 86	BOPOMOFO LETTER P
+U+3107	ㄇ	e3 84 87	BOPOMOFO LETTER M
+U+3108	ㄈ	e3 84 88	BOPOMOFO LETTER F
+U+3109	ㄉ	e3 84 89	BOPOMOFO LETTER D
+U+310A	ㄊ	e3 84 8a	BOPOMOFO LETTER T
+U+310B	ㄋ	e3 84 8b	BOPOMOFO LETTER N
+U+310C	ㄌ	e3 84 8c	BOPOMOFO LETTER L
+U+310D	ㄍ	e3 84 8d	BOPOMOFO LETTER G
+U+310E	ㄎ	e3 84 8e	BOPOMOFO LETTER K
+U+310F	ㄏ	e3 84 8f	BOPOMOFO LETTER H
+U+3110	ㄐ	e3 84 90	BOPOMOFO LETTER J
+U+3111	ㄑ	e3 84 91	BOPOMOFO LETTER Q
+U+3112	ㄒ	e3 84 92	BOPOMOFO LETTER X
+U+3113	ㄓ	e3 84 93	BOPOMOFO LETTER ZH
+U+3114	ㄔ	e3 84 94	BOPOMOFO LETTER CH
+U+3115	ㄕ	e3 84 95	BOPOMOFO LETTER SH
+U+3116	ㄖ	e3 84 96	BOPOMOFO LETTER R
+U+3117	ㄗ	e3 84 97	BOPOMOFO LETTER Z
+U+3118	ㄘ	e3 84 98	BOPOMOFO LETTER C
+U+3119	ㄙ	e3 84 99	BOPOMOFO LETTER S
+U+311A	ㄚ	e3 84 9a	BOPOMOFO LETTER A
+U+311B	ㄛ	e3 84 9b	BOPOMOFO LETTER O
+U+311C	ㄜ	e3 84 9c	BOPOMOFO LETTER E
+U+311D	ㄝ	e3 84 9d	BOPOMOFO LETTER EH
+U+311E	ㄞ	e3 84 9e	BOPOMOFO LETTER AI
+U+311F	ㄟ	e3 84 9f	BOPOMOFO LETTER EI
+U+3120	ㄠ	e3 84 a0	BOPOMOFO LETTER AU
+U+3121	ㄡ	e3 84 a1	BOPOMOFO LETTER OU
+U+3122	ㄢ	e3 84 a2	BOPOMOFO LETTER AN
+U+3123	ㄣ	e3 84 a3	BOPOMOFO LETTER EN
+U+3124	ㄤ	e3 84 a4	BOPOMOFO LETTER ANG
+U+3125	ㄥ	e3 84 a5	BOPOMOFO LETTER ENG
+U+3126	ㄦ	e3 84 a6	BOPOMOFO LETTER ER
+U+3127	ㄧ	e3 84 a7	BOPOMOFO LETTER I
+U+3128	ㄨ	e3 84 a8	BOPOMOFO LETTER U
+U+3129	ㄩ	e3 84 a9	BOPOMOFO LETTER IU
+
+EOM;
+
+//$title = '';
+
+$title .= <<<EOM
+
+ぁ 	あ 	ぃ 	い 	ぅ 	う 	ぇ 	え 	ぉ 	お 	か 	が 	き 	ぎ 	く
+U+305x 	ぐ 	け 	げ 	こ 	ご 	さ 	ざ 	し 	じ 	す 	ず 	せ 	ぜ 	そ 	ぞ 	た
+U+306x 	だ 	ち 	ぢ 	っ 	つ 	づ 	て 	で 	と 	ど 	な 	に 	ぬ 	ね 	の 	は
+U+307x 	ば 	ぱ 	ひ 	び 	ぴ 	ふ 	ぶ 	ぷ 	へ 	べ 	ぺ 	ほ 	ぼ 	ぽ 	ま 	み
+U+308x 	む 	め 	も 	ゃ 	や 	ゅ 	ゆ 	ょ 	よ 	ら 	り 	る 	れ 	ろ 	ゎ 	わ
+U+309x 	ゐ 	ゑ 	を 	ん 	ゔ 	ゕ 	ゖ 	  	  	゙ 	゚ 	゛ 	゜ 	ゝ 	ゞ 	ゟ
+
+片假名
+Unicode.org chart (PDF)
+  	0 	1 	2 	3 	4 	5 	6 	7 	8 	9 	A 	B 	C 	D 	E 	F
+U+30Ax 	゠ 	ァ 	ア 	ィ 	イ 	ゥ 	ウ 	ェ 	エ 	ォ 	オ 	カ 	ガ 	キ 	ギ 	ク
+U+30Bx 	グ 	ケ 	ゲ 	コ 	ゴ 	サ 	ザ 	シ 	ジ 	ス 	ズ 	セ 	ゼ 	ソ 	ゾ 	タ
+U+30Cx 	ダ 	チ 	ヂ 	ッ 	ツ 	ヅ 	テ 	デ 	ト 	ド 	ナ 	ニ 	ヌ 	ネ 	ノ 	ハ
+U+30Dx 	バ 	パ 	ヒ 	ビ 	ピ 	フ 	ブ 	プ 	ヘ 	ベ 	ペ 	ホ 	ボ 	ポ 	マ 	ミ
+U+30Ex 	ム 	メ 	モ 	ャ 	ヤ 	ュ 	ユ 	ョ 	ヨ 	ラ 	リ 	ル 	レ 	ロ 	ヮ 	ワ
+U+30Fx 	ヰ 	ヱ 	ヲ 	ン 	ヴ 	ヵ 	ヶ 	ヷ 	ヸ 	ヹ 	ヺ 	・ 	ー 	ヽ 	ヾ 	ヿ
+
+片假名語音擴展
+Unicode.org chart (PDF)
+  	0 	1 	2 	3 	4 	5 	6 	7 	8 	9 	A 	B 	C 	D 	E 	F
+U+31Fx 	ㇰ 	ㇱ 	ㇲ 	ㇳ 	ㇴ 	ㇵ 	ㇶ 	ㇷ 	ㇸ 	ㇹ 	ㇺ 	ㇻ 	ㇼ 	ㇽ 	ㇾ 	ㇿ
+
+漢語注音符號
+Unicode.org chart (PDF)
+  	0 	1 	2 	3 	4 	5 	6 	7 	8 	9 	A 	B 	C 	D 	E 	F
+U+310x 	  	  	  	  	  	ㄅ 	ㄆ 	ㄇ 	ㄈ 	ㄉ 	ㄊ 	ㄋ 	ㄌ 	ㄍ 	ㄎ 	ㄏ
+U+311x 	ㄐ 	ㄑ 	ㄒ 	ㄓ 	ㄔ 	ㄕ 	ㄖ 	ㄗ 	ㄘ 	ㄙ 	ㄚ 	ㄛ 	ㄜ 	ㄝ 	ㄞ 	ㄟ
+U+312x 	ㄠ 	ㄡ 	ㄢ 	ㄣ 	ㄤ 	ㄥ 	ㄦ 	ㄧ 	ㄨ 	ㄩ 	ㄪ 	ㄫ 	ㄬ 	ㄭ
+漢語注音符號擴展
+Unicode.org chart (PDF)
+  	0 	1 	2 	3 	4 	5 	6 	7 	8 	9 	A 	B 	C 	D 	E 	F
+U+31Ax 	ㆠ 	ㆡ 	ㆢ 	ㆣ 	ㆤ 	ㆥ 	ㆦ 	ㆧ 	ㆨ 	ㆩ 	ㆪ 	ㆫ 	ㆬ 	ㆭ 	ㆮ 	ㆯ
+U+31Bx 	ㆰ 	ㆱ 	ㆲ 	ㆳ 	ㆴ 	ㆵ 	ㆶ 	ㆷ 	ㆸ 	ㆹ 	ㆺ
+
+U+3220	㈠	e3 88 a0	PARENTHESIZED IDEOGRAPH ONE
+U+3221	㈡	e3 88 a1	PARENTHESIZED IDEOGRAPH TWO
+U+3222	㈢	e3 88 a2	PARENTHESIZED IDEOGRAPH THREE
+U+3223	㈣	e3 88 a3	PARENTHESIZED IDEOGRAPH FOUR
+U+3224	㈤	e3 88 a4	PARENTHESIZED IDEOGRAPH FIVE
+U+3225	㈥	e3 88 a5	PARENTHESIZED IDEOGRAPH SIX
+U+3226	㈦	e3 88 a6	PARENTHESIZED IDEOGRAPH SEVEN
+U+3227	㈧	e3 88 a7	PARENTHESIZED IDEOGRAPH EIGHT
+U+3228	㈨	e3 88 a8	PARENTHESIZED IDEOGRAPH NINE
+U+3229	㈩	e3 88 a9	PARENTHESIZED IDEOGRAPH TEN
+U+322A	㈪	e3 88 aa	PARENTHESIZED IDEOGRAPH MOON
+U+322B	㈫	e3 88 ab	PARENTHESIZED IDEOGRAPH FIRE
+U+322C	㈬	e3 88 ac	PARENTHESIZED IDEOGRAPH WATER
+U+322D	㈭	e3 88 ad	PARENTHESIZED IDEOGRAPH WOOD
+U+322E	㈮	e3 88 ae	PARENTHESIZED IDEOGRAPH METAL
+U+322F	㈯	e3 88 af	PARENTHESIZED IDEOGRAPH EARTH
+U+3230	㈰	e3 88 b0	PARENTHESIZED IDEOGRAPH SUN
+U+3231	㈱	e3 88 b1	PARENTHESIZED IDEOGRAPH STOCK
+U+3232	㈲	e3 88 b2	PARENTHESIZED IDEOGRAPH HAVE
+U+3233	㈳	e3 88 b3	PARENTHESIZED IDEOGRAPH SOCIETY
+U+3234	㈴	e3 88 b4	PARENTHESIZED IDEOGRAPH NAME
+U+3235	㈵	e3 88 b5	PARENTHESIZED IDEOGRAPH SPECIAL
+U+3236	㈶	e3 88 b6	PARENTHESIZED IDEOGRAPH FINANCIAL
+U+3237	㈷	e3 88 b7	PARENTHESIZED IDEOGRAPH CONGRATULATION
+U+3238	㈸	e3 88 b8	PARENTHESIZED IDEOGRAPH LABOR
+U+3239	㈹	e3 88 b9	PARENTHESIZED IDEOGRAPH REPRESENT
+U+323A	㈺	e3 88 ba	PARENTHESIZED IDEOGRAPH CALL
+U+323B	㈻	e3 88 bb	PARENTHESIZED IDEOGRAPH STUDY
+U+323C	㈼	e3 88 bc	PARENTHESIZED IDEOGRAPH SUPERVISE
+U+323D	㈽	e3 88 bd	PARENTHESIZED IDEOGRAPH ENTERPRISE
+U+323E	㈾	e3 88 be	PARENTHESIZED IDEOGRAPH RESOURCE
+U+323F	㈿	e3 88 bf	PARENTHESIZED IDEOGRAPH ALLIANCE
+U+3240	㉀	e3 89 80	PARENTHESIZED IDEOGRAPH FESTIVAL
+U+3241	㉁	e3 89 81	PARENTHESIZED IDEOGRAPH REST
+U+3242	㉂	e3 89 82	PARENTHESIZED IDEOGRAPH SELF
+U+3243	㉃	e3 89 83	PARENTHESIZED IDEOGRAPH REACH
+U+3244	㉄	e3 89 84	CIRCLED IDEOGRAPH QUESTION
+U+3245	㉅	e3 89 85	CIRCLED IDEOGRAPH KINDERGARTEN
+U+3246	㉆	e3 89 86	CIRCLED IDEOGRAPH SCHOOL
+U+3247	㉇	e3 89 87	CIRCLED IDEOGRAPH KOTO
+U+3248	㉈	e3 89 88	CIRCLED NUMBER TEN ON BLACK SQUARE
+U+3249	㉉	e3 89 89	CIRCLED NUMBER TWENTY ON BLACK SQUARE
+U+324A	㉊	e3 89 8a	CIRCLED NUMBER THIRTY ON BLACK SQUARE
+U+324B	㉋	e3 89 8b	CIRCLED NUMBER FORTY ON BLACK SQUARE
+U+324C	㉌	e3 89 8c	CIRCLED NUMBER FIFTY ON BLACK SQUARE
+U+324D	㉍	e3 89 8d	CIRCLED NUMBER SIXTY ON BLACK SQUARE
+U+324E	㉎	e3 89 8e	CIRCLED NUMBER SEVENTY ON BLACK SQUARE
+U+324F	㉏	e3 89 8f	CIRCLED NUMBER EIGHTY ON BLACK SQUARE
+U+3250	㉐	e3 89 90	PARTNERSHIP SIGN
+U+3251	㉑	e3 89 91	CIRCLED NUMBER TWENTY ONE
+U+3252	㉒	e3 89 92	CIRCLED NUMBER TWENTY TWO
+U+3253	㉓	e3 89 93	CIRCLED NUMBER TWENTY THREE
+U+3254	㉔	e3 89 94	CIRCLED NUMBER TWENTY FOUR
+U+3255	㉕	e3 89 95	CIRCLED NUMBER TWENTY FIVE
+U+3256	㉖	e3 89 96	CIRCLED NUMBER TWENTY SIX
+U+3257	㉗	e3 89 97	CIRCLED NUMBER TWENTY SEVEN
+U+3258	㉘	e3 89 98	CIRCLED NUMBER TWENTY EIGHT
+U+3259	㉙	e3 89 99	CIRCLED NUMBER TWENTY NINE
+U+325A	㉚	e3 89 9a	CIRCLED NUMBER THIRTY
+U+325B	㉛	e3 89 9b	CIRCLED NUMBER THIRTY ONE
+U+325C	㉜	e3 89 9c	CIRCLED NUMBER THIRTY TWO
+U+325D	㉝	e3 89 9d	CIRCLED NUMBER THIRTY THREE
+U+325E	㉞	e3 89 9e	CIRCLED NUMBER THIRTY FOUR
+U+325F	㉟	e3 89 9f	CIRCLED NUMBER THIRTY FIVE
+U+3260	㉠	e3 89 a0	CIRCLED HANGUL KIYEOK
+U+3261	㉡	e3 89 a1	CIRCLED HANGUL NIEUN
+U+3262	㉢	e3 89 a2	CIRCLED HANGUL TIKEUT
+U+3263	㉣	e3 89 a3	CIRCLED HANGUL RIEUL
+U+3264	㉤	e3 89 a4	CIRCLED HANGUL MIEUM
+U+3265	㉥	e3 89 a5	CIRCLED HANGUL PIEUP
+U+3266	㉦	e3 89 a6	CIRCLED HANGUL SIOS
+U+3267	㉧	e3 89 a7	CIRCLED HANGUL IEUNG
+U+3268	㉨	e3 89 a8	CIRCLED HANGUL CIEUC
+U+3269	㉩	e3 89 a9	CIRCLED HANGUL CHIEUCH
+U+326A	㉪	e3 89 aa	CIRCLED HANGUL KHIEUKH
+U+326B	㉫	e3 89 ab	CIRCLED HANGUL THIEUTH
+U+326C	㉬	e3 89 ac	CIRCLED HANGUL PHIEUPH
+U+326D	㉭	e3 89 ad	CIRCLED HANGUL HIEUH
+U+326E	㉮	e3 89 ae	CIRCLED HANGUL KIYEOK A
+U+326F	㉯	e3 89 af	CIRCLED HANGUL NIEUN A
+U+3270	㉰	e3 89 b0	CIRCLED HANGUL TIKEUT A
+U+3271	㉱	e3 89 b1	CIRCLED HANGUL RIEUL A
+U+3272	㉲	e3 89 b2	CIRCLED HANGUL MIEUM A
+U+3273	㉳	e3 89 b3	CIRCLED HANGUL PIEUP A
+U+3274	㉴	e3 89 b4	CIRCLED HANGUL SIOS A
+U+3275	㉵	e3 89 b5	CIRCLED HANGUL IEUNG A
+U+3276	㉶	e3 89 b6	CIRCLED HANGUL CIEUC A
+U+3277	㉷	e3 89 b7	CIRCLED HANGUL CHIEUCH A
+U+3278	㉸	e3 89 b8	CIRCLED HANGUL KHIEUKH A
+U+3279	㉹	e3 89 b9	CIRCLED HANGUL THIEUTH A
+U+327A	㉺	e3 89 ba	CIRCLED HANGUL PHIEUPH A
+U+327B	㉻	e3 89 bb	CIRCLED HANGUL HIEUH A
+U+327C	㉼	e3 89 bc	CIRCLED KOREAN CHARACTER CHAMKO
+U+327D	㉽	e3 89 bd	CIRCLED KOREAN CHARACTER JUEUI
+U+327E	㉾	e3 89 be	CIRCLED HANGUL IEUNG U
+U+327F	㉿	e3 89 bf	KOREAN STANDARD SYMBOL
+U+3280	㊀	e3 8a 80	CIRCLED IDEOGRAPH ONE
+U+3281	㊁	e3 8a 81	CIRCLED IDEOGRAPH TWO
+U+3282	㊂	e3 8a 82	CIRCLED IDEOGRAPH THREE
+U+3283	㊃	e3 8a 83	CIRCLED IDEOGRAPH FOUR
+U+3284	㊄	e3 8a 84	CIRCLED IDEOGRAPH FIVE
+U+3285	㊅	e3 8a 85	CIRCLED IDEOGRAPH SIX
+U+3286	㊆	e3 8a 86	CIRCLED IDEOGRAPH SEVEN
+U+3287	㊇	e3 8a 87	CIRCLED IDEOGRAPH EIGHT
+U+3288	㊈	e3 8a 88	CIRCLED IDEOGRAPH NINE
+U+3289	㊉	e3 8a 89	CIRCLED IDEOGRAPH TEN
+U+328A	㊊	e3 8a 8a	CIRCLED IDEOGRAPH MOON
+U+328B	㊋	e3 8a 8b	CIRCLED IDEOGRAPH FIRE
+U+328C	㊌	e3 8a 8c	CIRCLED IDEOGRAPH WATER
+U+328D	㊍	e3 8a 8d	CIRCLED IDEOGRAPH WOOD
+U+328E	㊎	e3 8a 8e	CIRCLED IDEOGRAPH METAL
+U+328F	㊏	e3 8a 8f	CIRCLED IDEOGRAPH EARTH
+
+EOM;
+
+foreach (array(
+	'e3',
+	'80',
+	'd6',
+	'3041',
+	'3096',
+	'30',
+	'41',
+	'96',
+) as $_k) {
+
+	echo $_k . ' = ' . hexdec($_k).'<br>';
+
+}
+
+// utf8 平假名
+$r = '/(\xe3(?:\x81[\x81-\xbf]|\x82[\x80-\x96\x99-\x9f]))/';
+// 片假名
+$r = '/(\xe3(?:\x82[\xa0-\xbf]|\x83[\x80-\xbf]))/';
+// 注音
+$r = '/(\xe3(?:\x84[\x85-\xa9]))/';
+
+//for($j=hexdec('81'); $j<=(hexdec('81')+85); $j++) {
+	unset($m);
+
+	$r = '/(\xe3(?:\x81[\x81-\xbf]|\x82[\x80-\x96\x99-\x9f]))/';
+	$r = '/(\xe3(?:\x82[\xa0-\xbf]|\x83[\x80-\xbf]))/';
+	$r = '/(\xe3(?:\x84[\x85-\xa9]))/';
+
+	preg_match_all($r, $title, $m);
+
+	if (!empty($m[0])) {
+
+		$m[0] = array_unique($m[0]);
+
+		sort($m[0]);
+
+		echo '<hr>'.$r.'<br>';
+
+		foreach ($m[0] as $_k => $_v) {
+			echo $_k . ' = '
+				. '<b style="color: red;">'
+				. $_v
+				. '</b>'
+				. ' = '
+				. ord($_v[0])
+				. ','
+				. ord($_v[1])
+				. ','
+				. ord($_v[2])
+				. ' ; '
+				. dechex(ord($_v[0]))
+				. ' '
+				. dechex(ord($_v[1]))
+				. ' '
+				. dechex(ord($_v[2]))
+				. '<br>'
+			;
+		}
+	}
+//}
+
+exit();
+
+$i = 0;
+
+echo '<hr>'.$i++;
+
+preg_match_all('/([\x{3040}-\x{309F}])/u', $title, $m);
+
+print_r($m);
+
+echo '<hr>'.$i++;
+
+unset($m);
+
+preg_match_all('/([\x{3040}-\x{309F}]|[\x7f-\xff]+)/u', $title, $m);
+
+print_r($m);
+
+echo '<hr>'.$i++;
+
+unset($m);
+
+preg_match_all('/([\xc0-\xdf][\x80-\xbf]|[\xe0-\xef][\x80-\xbf]{2}|[\xf0-\xff][\x80-\xbf]{3})/', $title, $m);
+
+print_r($m);
+
+echo '<hr>'.$i++;
+
+unset($m);
+
+preg_match_all('/([\x01-\x7f])/', $title, $m);
+
+print_r($m);
+
+echo '<hr>'.$i++;
+
+unset($m);
+
+preg_match_all('/([\xc0-\xdf][\x80-\xbf])/', $title, $m);
+
+print_r($m);
+
+echo '<hr>'.$i++;
+
+unset($m);
+
+preg_match_all('/([\xe0-\xef][\x80-\xbf]{2})/', $title, $m);
+
+print_r($m);
+
+echo '<hr>'.$i++;
+
+unset($m);
+
+preg_match_all('/([\xf0-\xff][\x80-\xbf]{3})/', $title, $m);
+
+print_r($m);
+
+echo '<hr>'.$i++;
+
+?>
