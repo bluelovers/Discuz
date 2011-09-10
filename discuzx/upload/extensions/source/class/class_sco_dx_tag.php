6a0e3a8a3957c80edad75765d77322aca6ab34c8
bluelovers <codelovers@users.sourceforge.net>
2011-09-10 16:59:04 +0800

    copy from function_forum.php

--- discuzx/upload/extensions/source/class/class_sco_dx_tag.php
+++ discuzx/upload/extensions/source/class/class_sco_dx_tag.php
@@ -7,6 +7,17 @@
 
 class _sco_dx_tag {
 
+	function tagarray($tags) {
+		$tags = str_replace(array(chr(0xa3).chr(0xac), chr(0xa1).chr(0x41), chr(0xef).chr(0xbc).chr(0x8c)), ',', censor($tags));
+		if(strexists($tags, ',')) {
+			$tagarray = array_unique(explode(',', $tags));
+		} else {
+			$langcore = lang('core');
+			$tags = str_replace($langcore['fullblankspace'], ' ', $tags);
+			$tagarray = array_unique(explode(' ', $tags));
+		}
+	}
+
 	function utf8_strlen($str) {
 		return preg_match_all('/[\x00-\x7F\xC0-\xFD]/', $str, $dummy);
 	}
