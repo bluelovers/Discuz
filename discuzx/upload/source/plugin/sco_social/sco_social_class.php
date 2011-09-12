fbed1103d8a57d97a573b2087e141d028b5977d8
bluelovers <codelovers@users.sourceforge.net>
2011-09-13 00:50:38 +0800

    class plugin_sco_social extends _sco_dx_plugin

--- discuzx/upload/source/plugin/sco_social/sco_social_class.php
+++ discuzx/upload/source/plugin/sco_social/sco_social_class.php
@@ -5,6 +5,17 @@
  * @copyright 2011
  */
 
+include_once libfile('class/sco_dx_plugin', 'source', 'extensions/');
 
+class plugin_sco_social extends _sco_dx_plugin {
+
+	public function __construct() {
+		$this->_init($this->_get_identifier(__CLASS__));
+
+		// set instance = $this
+		$this->_this(&$this);
+	}
+
+}
 
 ?>
