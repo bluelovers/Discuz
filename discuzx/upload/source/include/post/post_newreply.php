5a6d0763e8c3eac15affcff8ee63f5e1ce0d24ec
bluelovers <codelovers@users.sourceforge.net>
2011-09-15 11:36:56 +0800

    $post['pid']

--- discuzx/upload/source/include/post/post_newreply.php
+++ discuzx/upload/source/include/post/post_newreply.php
@@ -122,7 +122,7 @@ if($_G['setting']['commentnumber'] && !empty($_G['gp_comment'])) {
 		) {
 			notification_add($thread['authorid'], 'pcomment', 'comment_add', array(
 				'tid' => $_G['tid'],
-				'pid' => $_G['gp_pid'],
+				'pid' => $post['pid'],
 				'subject' => $thread['subject'],
 				'commentmsg' => cutstr(str_replace(array('[b]', '[/b]', '[/color]'), '', preg_replace("/\[color=([#\w]+?)\]/i", "", stripslashes($comment))), 200)
 			));
