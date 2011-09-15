dd2dfd9403f839b82710d398b0be6638772f30f2
bluelovers <codelovers@users.sourceforge.net>
2011-09-15 11:38:18 +0800

    $_commentmsg

--- discuzx/upload/source/include/post/post_newreply.php
+++ discuzx/upload/source/include/post/post_newreply.php
@@ -145,7 +145,7 @@ if($_G['setting']['commentnumber'] && !empty($_G['gp_comment'])) {
 					'tid' => $_G['tid'],
 					'pid' => $post['pid'],
 					'subject' => $thread['subject'],
-					'commentmsg' => cutstr(str_replace(array('[b]', '[/b]', '[/color]'), '', preg_replace("/\[color=([#\w]+?)\]/i", "", stripslashes($comment))), 200)
+					'commentmsg' => $_commentmsg
 				));
 			}
 		}
