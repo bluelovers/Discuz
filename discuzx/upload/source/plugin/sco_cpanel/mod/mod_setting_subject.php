d2de3c485f35eb5319b6437ee4ec3d2c9d8736ca
bluelovers <codelovers@users.sourceforge.net>
2011-09-16 22:01:34 +0800

    class plugin_sco_cpanel_setting_subject extends plugin_sco_cpanel

---
+++ discuzx/upload/source/plugin/sco_cpanel/mod/mod_setting_subject.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2011
+ */
+
+if (!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+class plugin_sco_cpanel_setting_subject extends plugin_sco_cpanel {
+
+}
+
+?>
