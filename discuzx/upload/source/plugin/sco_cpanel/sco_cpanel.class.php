e01adcc2e84b130c11105308f3da3dae681f5fc8
bluelovers <codelovers@users.sourceforge.net>
2011-09-16 22:22:24 +0800

    class plugin_sco_cpanel extends _sco_dx_plugin_admincp

---
+++ discuzx/upload/source/plugin/sco_cpanel/sco_cpanel.class.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2011
+ */
+
+if (!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+include_once dirname(__FILE__).'/./class_sco_dx_plugin_admincp.php';
+
+class plugin_sco_cpanel extends _sco_dx_plugin_admincp {
+
+}
+
+?>
