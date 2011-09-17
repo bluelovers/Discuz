d67580891e73122734e3e235ecc1dc041761c98b
bluelovers <codelovers@users.sourceforge.net>
2011-09-17 23:35:40 +0800

    post_subject_maxsize

--- discuzx/upload/static/js/forum_post.js
+++ discuzx/upload/static/js/forum_post.js
@@ -86,7 +86,7 @@ function validate(theform) {
 		showError('抱歉，您尚未輸入標題或內容');
 		return false;
 	} else if(mb_strlen(theform.subject.value) > 80) {
-		showError('您的標題超過 80 個字符的限制');
+		showError('您的標題超過 ' + post_subject_maxsize + ' 個字符的限制');
 		return false;
 	}
 	if(ispicstyleforum == 1 && ATTACHORIMAGE == 0 && isfirstpost) {
