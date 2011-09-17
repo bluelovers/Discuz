d1af5830706d89c64a0f4a3dec8942d7b64d7a94
bluelovers <codelovers@users.sourceforge.net>
2011-09-17 10:38:08 +0800

    function strlen($str)

--- discuzx/upload/source/plugin/sco_cpanel/mod/mod_setting_subject.php
+++ discuzx/upload/source/plugin/sco_cpanel/mod/mod_setting_subject.php
@@ -11,6 +11,15 @@ if (!defined('IN_DISCUZ')) {
 
 class plugin_sco_cpanel_setting_subject extends plugin_sco_cpanel {
 
+	function strlen($str) {
+		if(strtolower(CHARSET) == 'utf-8') return self::utf8_strlen($str);
+
+		return dstrlen($str);
+	}
+
+	function utf8_strlen($str) {
+		return preg_match_all('/[\x00-\x7F\xC0-\xFD]/', $str, $dummy);
+	}
 }
 
 ?>
