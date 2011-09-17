5aee01939b0d2bdc5de717f1566828543a2c2dba
bluelovers <codelovers@users.sourceforge.net>
2011-09-17 23:14:26 +0800

    $ret = $this->_db()->query("REPLACE INTO ".$this->_db()->table_name('common_setting')." SET skey='{$_k}', svalue='{$settingnew[$_k]}'");

--- discuzx/upload/source/plugin/sco_cpanel/mod/mod_setting_subject.php
+++ discuzx/upload/source/plugin/sco_cpanel/mod/mod_setting_subject.php
@@ -118,6 +118,15 @@ class plugin_sco_cpanel_setting_subject extends plugin_sco_cpanel {
 				}
 			}
 
+			foreach(array(
+				'post_subject_maxsize',
+				'post_subject_maxsize_blog',
+			) as $_k) {
+				if ($settingnew[$_k] != $setting[$_k]) {
+					$ret = $this->_db()->query("REPLACE INTO ".$this->_db()->table_name('common_setting')." SET skey='{$_k}', svalue='{$settingnew[$_k]}'");
+				}
+			}
+
 			if ($settingnew['post_subject_maxsize'] != $setting['post_subject_maxsize']) {
 				$ret = $this->_db()->query("REPLACE INTO ".$this->_db()->table_name('common_setting')." SET skey='post_subject_maxsize', svalue='{$settingnew[post_subject_maxsize]}'");
 
