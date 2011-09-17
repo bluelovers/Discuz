dcf828ce2818481cb56a879332be23f4128f3454
bluelovers <codelovers@users.sourceforge.net>
2011-09-17 23:18:47 +0800

    $POST['subject'] = getstr(trim($POST['subject']), $_G['setting']['post_subject_maxsize_blog'], 1, 1);

--- discuzx/upload/source/function/function_blog.php
+++ discuzx/upload/source/function/function_blog.php
@@ -26,7 +26,7 @@ function blog_post($POST, $olds=array()) {
 	$_G['setting']['post_subject_maxsize_blog'] = max(80, intval($_G['setting']['post_subject_maxsize_blog']));
 	// bluelovers
 
-	$POST['subject'] = getstr(trim($POST['subject']), 80, 1, 1);
+	$POST['subject'] = getstr(trim($POST['subject']), $_G['setting']['post_subject_maxsize_blog'], 1, 1);
 	$POST['subject'] = censor($POST['subject']);
 	if(strlen($POST['subject'])<1) $POST['subject'] = dgmdate($_G['timestamp'], 'Y-m-d');
 	$POST['friend'] = intval($POST['friend']);
