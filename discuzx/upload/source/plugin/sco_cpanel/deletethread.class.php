fe7f43e2ed2cd6f9110d58b478ae98cf9df89da6
bluelovers <codelovers@users.sourceforge.net>
2011-09-18 20:35:41 +0800

    function deletethread($_args = array())

--- discuzx/upload/source/plugin/sco_cpanel/deletethread.class.php
+++ discuzx/upload/source/plugin/sco_cpanel/deletethread.class.php
@@ -13,6 +13,10 @@ include_once libfile('class/sco_dx_plugin', 'source', 'extensions/');
 
 class plugin_sco_cpanel_delete extends _sco_dx_plugin {
 
+	function deletethread($_args = array()) {
+
+	}
+
 }
 
 class mobileplugin_sco_cpanel_delete extends plugin_sco_cpanel_delete {
