fda3c407e164aa27aabe6b5af4286ce47c7d18aa
bluelovers <codelovers@users.sourceforge.net>
2011-09-18 21:34:33 +0800

    .

--- discuzx/upload/source/plugin/sco_cpanel/sco_cpanel.class.php
+++ discuzx/upload/source/plugin/sco_cpanel/sco_cpanel.class.php
@@ -14,7 +14,26 @@ include_once libfile('class/sco_dx_plugin', 'source', 'extensions/');
 class plugin_sco_cpanel extends _sco_dx_plugin {
 
 	function deletethread($_args = array()) {
-		debug($_args);
+/*
+Array
+(
+    [param] => Array
+        (
+            [0] => Array
+                (
+                    [0] => 32989
+                )
+
+            [1] => 1
+            [2] => 1
+            [3] => 1
+        )
+
+    [step] => check
+)
+*/
+
+		list($tids, $membercount, $credit, $ponly) = $_args['param'];
 	}
 
 }
