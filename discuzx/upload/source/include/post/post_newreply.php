9d0c2bc466997462f64e06789491370f0231d51e
bluelovers <codelovers@users.sourceforge.net>
2011-09-19 23:39:00 +0800

    if (!$_uid ||

--- discuzx/upload/source/include/post/post_newreply.php
+++ discuzx/upload/source/include/post/post_newreply.php
@@ -158,7 +158,7 @@ if($_G['setting']['commentnumber'] && !empty($_G['gp_comment'])) {
 	$_user_list = array_unique($_user_list);
 
 	foreach ($_user_list as $_uid) {
-		if ($_uid == $_G['uid']) continue;
+		if (!$_uid || $_uid == $_G['uid']) continue;
 
 		// bluelovers
 
