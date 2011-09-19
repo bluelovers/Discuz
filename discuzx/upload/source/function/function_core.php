2972a1f83d856765dc33342a492b4502c6e23bc9
bluelovers <codelovers@users.sourceforge.net>
2011-09-19 14:23:23 +0800

    .

--- discuzx/upload/source/function/function_core.php
+++ discuzx/upload/source/function/function_core.php
@@ -1401,14 +1401,39 @@ function cutstr($string, $length, $dot = ' ...') {
 				$tn = 1; $n++; $noc++;
 			} elseif(194 <= $t && $t <= 223) {
 				$tn = 2; $n += 2; $noc += 2;
+
+				// bluelovers
+				$noc -= 1;
+				// bluelovers
+
 			} elseif(224 <= $t && $t <= 239) {
 				$tn = 3; $n += 3; $noc += 2;
+
+				// bluelovers
+				$noc -= 1;
+				// bluelovers
+
 			} elseif(240 <= $t && $t <= 247) {
 				$tn = 4; $n += 4; $noc += 2;
+
+				// bluelovers
+				$noc -= 1;
+				// bluelovers
+
 			} elseif(248 <= $t && $t <= 251) {
 				$tn = 5; $n += 5; $noc += 2;
+
+				// bluelovers
+				$noc -= 1;
+				// bluelovers
+
 			} elseif($t == 252 || $t == 253) {
 				$tn = 6; $n += 6; $noc += 2;
+
+				// bluelovers
+				$noc -= 1;
+				// bluelovers
+
 			} else {
 				$n++;
 			}
