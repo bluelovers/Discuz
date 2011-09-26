b7112dcfde54b601a349a2460292568eb0ab42cc
bluelovers <codelovers@users.sourceforge.net>
2011-09-26 19:56:14 +0800

    is_true(func_get_arg(1))

--- discuzx/upload/extensions/source/function/function_core.php
+++ discuzx/upload/extensions/source/function/function_core.php
@@ -140,7 +140,7 @@ function include_file_once() {
 
 		$_cahce_include[func_get_arg(0)] = include_once(func_get_arg(0));
 
-		if (true === func_get_arg(1) || 1 === func_get_arg(1)) {
+		if (is_true(func_get_arg(1))) {
 			return get_runtime_defined_vars(get_defined_vars(), array(
 				'_G',
 				'_cahce_include',
