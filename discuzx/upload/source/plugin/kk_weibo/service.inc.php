4785c2ecad2961168c2df4d29eca1f43d0597d25
bluelovers <codelovers@users.sourceforge.net>
2011-09-27 00:41:40 +0800

    .

--- discuzx/upload/source/plugin/kk_weibo/service.inc.php
+++ discuzx/upload/source/plugin/kk_weibo/service.inc.php
@@ -1,6 +1,7 @@
 <?php
     if(!defined('IN_DISCUZ')) exit('Access Denied');
 	if(empty($_G['uid'])) showmessage('请登录后操作');
+	$kk_weibo=$_G['cache']['plugin']['kk_weibo'];
 	//-------------------------------------------------------------------------------------------------
 	function kk_weibo_check() {
 		global $action,$uid,$uid_rel;
@@ -25,6 +26,10 @@
 		$data=Array('uid'=>$uid,'count_attention'=>$count['t_count']);
 		if(empty($row)) DB::insert($table_stat_s,$data); else DB::update($table_stat_s,$data,"uid={$uid}");		
 	}
+	function kk_weibo_sendnotify($to_uid,$tpl,$param) {		
+		foreach($param as $key=>$value) $tpl=str_replace('{'.$key.'}',$value,$tpl);
+		notification_add($to_uid,9696,$tpl,'',1);
+	}
 	//-------------------------------------------------------------------------------------------------
 	$action=$_G['gp_action']; $uid=$_G['uid']; $uid_rel=(int)$_G['gp_rel'];
 	kk_weibo_check();
@@ -36,15 +41,29 @@
 		if(empty($row)) {			
 			DB::query("insert into {$table}(uid,uid_rel) values({$uid},{$uid_rel})");
 			kk_weibo_update($uid_rel);
+			if($kk_weibo['send_notify']) {
+				$from_user=getuserbyuid($uid);
+				kk_weibo_sendnotify($uid_rel,$kk_weibo['send_notify_tpl'],Array(
+					'uid'		=> $uid,
+					'username' 	=> $from_user['username'],
+					'url_fans'	=> '/plugin.php?id=kk_weibo:weibo&view=fans',
+				));
+			}
+			$description="添加关注成功。";
+		}else {
+			$description="您之前已经关注过 他/她 了，不能重覆关注。";
 		}
-		$description="添加关注成功";			
+				
 	} else if($action=='del') {		
 		$row=kk_weibo_fetch($uid_rel);
 		if(!empty($row)) {			
 			DB::query("delete from {$table} where uid={$uid} and uid_rel={$uid_rel}");			
 			kk_weibo_update($uid_rel);
+			$description="取消关注成功。";
+		}else {
+			$description="您都还没有关注 他/她，谈何取消呢？";
 		}
-		$description="取消关注成功";
+		
 	}
 	
 	showmessage($description,
