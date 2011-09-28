a52fa4f157f558e5d4ba72d043082d99f3093808
bluelovers <codelovers@users.sourceforge.net>
2011-09-28 22:32:01 +0800

    function &run()

--- discuzx/upload/source/plugin/sco_plugin/class_sco_plugin_inc.php
+++ discuzx/upload/source/plugin/sco_plugin/class_sco_plugin_inc.php
@@ -13,6 +13,14 @@ include_once dirname(__FILE__).'/./class_sco_dx_plugin_inc.php';
 
 class plugin_sco_plugin_inc extends _sco_dx_plugin_inc {
 
+	function &run() {
+		$this->_setglobal('mod_lists', $this->_get_mod_list());
+
+		parent::run();
+
+		return $this;
+	}
+
 }
 
 ?>
