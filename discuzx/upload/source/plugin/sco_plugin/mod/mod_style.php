d0afeb1b79234f2faef7cbbce5a2f61674471a54
bluelovers <codelovers@users.sourceforge.net>
2011-09-28 21:49:40 +0800

    function _my_get_stylelist()

--- discuzx/upload/source/plugin/sco_plugin/mod/mod_style.php
+++ discuzx/upload/source/plugin/sco_plugin/mod/mod_style.php
@@ -15,6 +15,40 @@ class plugin_sco_plugin_style extends plugin_sco_plugin_inc {
 
 	}
 
+	function _my_get_stylelist() {
+		global $_G;
+
+		$sarray = array();
+		$styleid_default = $_G['setting']['styleid'];
+
+		$query = DB::query("SELECT
+				s.styleid, s.available, s.name, t.name AS tplname, t.directory, t.copyright
+			FROM ".DB::table('common_style')." s
+			LEFT JOIN ".DB::table('common_template')." t ON t.templateid=s.templateid
+			WHERE
+				s.available = '1'
+			ORDER BY
+				s.available desc
+				, s.styleid = '$styleid_default' DESC
+				, s.name ASC
+				, s.styleid
+		");
+
+		while($style = DB::fetch($query)) {
+
+			$preview = file_exists($style['directory'].'/preview.jpg') ? $style['directory'].'/preview.jpg' : './static/image/admincp/stylepreview.gif';
+			$previewlarge = file_exists($style['directory'].'/preview_large.jpg') ? $style['directory'].'/preview_large.jpg' : '';
+
+			$style['preview'] = $preview;
+			$style['previewlarge'] = $previewlarge;
+
+			$sarray[$style['styleid']] = $style;
+		}
+
+		$this->_setglobal('style_defaultid', $styleid_default);
+		$this->_setglobal('style_lists', $sarray);
+	}
+
 }
 
 ?>
