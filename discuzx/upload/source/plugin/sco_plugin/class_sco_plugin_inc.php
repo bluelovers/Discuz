00aeb11db694bf57b8384429ed772ccedeb93bb1
bluelovers <codelovers@users.sourceforge.net>
2011-09-28 17:31:42 +0800

    class plugin_sco_plugin_inc extends _sco_dx_plugin_inc

---
+++ discuzx/upload/source/plugin/sco_plugin/class_sco_plugin_inc.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2011
+ */
+
+if (!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+include_once dirname(__FILE__).'/./class_sco_dx_plugin_inc.php';
+
+class plugin_sco_plugin_inc extends _sco_dx_plugin_inc {
+
+}
+
+?>
