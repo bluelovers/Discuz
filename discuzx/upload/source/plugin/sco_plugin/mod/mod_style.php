4deea5cf34b6e83983e1f61942b4b2214b043e3b
bluelovers <codelovers@users.sourceforge.net>
2011-09-28 21:48:29 +0800

    class plugin_sco_plugin_style extends plugin_sco_plugin_inc

---
+++ discuzx/upload/source/plugin/sco_plugin/mod/mod_style.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2011
+ */
+
+if (!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+class plugin_sco_plugin_style extends plugin_sco_plugin_inc {
+
+	function on_op_default() {
+
+	}
+
+}
+
+?>
