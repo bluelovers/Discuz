afa562df0124747dffa488f1bf3a70d9ded785c6
bluelovers <codelovers@users.sourceforge.net>
2011-09-28 17:39:23 +0800

    plugin_sco_plugin_inc()

---
+++ discuzx/upload/source/plugin/sco_plugin/sco_plugin.inc.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2011
+ */
+
+if (!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+//error_reporting(E_ALL ^E_NOTICE ^E_STRICT);
+
+include_once dirname(__FILE__).'/./class_sco_plugin_inc.php';
+
+if (empty($_G['gp_cpmod'])) {
+
+	$_cpanel = new plugin_sco_plugin_inc();
+	$_cpanel
+		->init($plugin['identifier'])
+		->set(array(
+			'cpmod' => $_G['gp_cpmod'],
+			'module' => &$module,
+		))
+		->run()
+	;
+
+} else {
+
+	$_cpanel = plugin_sco_plugin_inc::mod($_G['gp_cpmod'], $plugin['identifier']);
+
+	$_cpanel
+		->set(array(
+			'op' => $_G['gp_op'],
+			'module' => &$module,
+		))
+		->run()
+	;
+
+}
+
+?>
