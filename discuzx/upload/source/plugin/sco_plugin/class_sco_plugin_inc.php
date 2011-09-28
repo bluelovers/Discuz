b6236515d0e5a2cb00a18d6fe964e0632eddecf3
bluelovers <codelovers@users.sourceforge.net>
2011-09-28 16:43:22 +0800

    function &init($identifier)

--- discuzx/upload/source/plugin/sco_plugin/class_sco_plugin_inc.php
+++ discuzx/upload/source/plugin/sco_plugin/class_sco_plugin_inc.php
@@ -13,6 +13,16 @@ include_once libfile('class/sco_dx_plugin', 'source', 'extensions/');
 
 class _sco_dx_plugin_inc extends _sco_dx_plugin {
 
+	function &init($identifier) {
+		$this->_init($identifier);
+
+		$this->_this(&$this);
+
+		$this->_fix_plugin_setting();
+
+		return $this;
+	}
+
 }
 
 ?>
