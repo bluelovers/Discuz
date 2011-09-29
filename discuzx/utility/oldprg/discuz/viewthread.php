526d82332fad3699a134fb4bf08fd9d09c7651dc
bluelovers <codelovers@users.sourceforge.net>
2011-09-30 03:49:32 +0800

    $url .= '&'.http_build_query($_GET);

--- discuzx/utility/oldprg/discuz/viewthread.php
+++ discuzx/utility/oldprg/discuz/viewthread.php
@@ -7,9 +7,17 @@ $url = 'forum.php?';
 
 if(is_numeric($_GET['tid'])) {
 	$url .= 'mod=viewthread&tid='.$_GET['tid'];
+	/*
 	if(is_numeric($_GET['page'])) {
 		$url .= '&page='.$_GET['page'];
 	}
+	*/
+	// bluelovers
+	unset($_GET['tid']);
+	unset($_GET['mod']);
+
+	$url .= '&'.http_build_query($_GET);
+	// bluelovers
 }
 
 header("location: $url");
