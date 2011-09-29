99408b64b546d3f5ff7bd1009fdf6e066f9acda6
bluelovers <codelovers@users.sourceforge.net>
2011-09-30 05:00:11 +0800

    [bugfix] 修正網址參數會變成 0 的意外BUG

--- discuzx/utility/oldprg/discuz/redirect.php
+++ discuzx/utility/oldprg/discuz/redirect.php
@@ -7,6 +7,10 @@ $pid = isset($_GET['pid']) ? intval($_GET['pid']) : 0;
 $ptid = !empty($_GET['tid']) ? intval($_GET['tid']) : (isset($_GET['ptid']) ? intval($_GET['ptid']) : 0);
 $goto = isset($_GET['goto']) ? $_GET['goto'] : '';
 
+/**
+ * 修正網址參數會變成 0 的意外BUG
+ * forum.php?mod=redirect&goto=lastpost&ptid=0&pid=0
+ */
 $url = 'forum.php?mod=redirect&goto='."{$goto}&ptid={$ptid}&pid={$pid}";
 
 header("location: $url");
