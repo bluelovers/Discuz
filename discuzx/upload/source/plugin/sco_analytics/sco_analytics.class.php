eddb4400f2d5dca2dd4f8843ada010c23255afc7
bluelovers <codelovers@users.sourceforge.net>
2011-10-02 00:19:30 +0800

    if (defined('IN_MOBILE')) return $ret;

--- discuzx/upload/source/plugin/sco_analytics/sco_analytics.class.php
+++ discuzx/upload/source/plugin/sco_analytics/sco_analytics.class.php
@@ -40,6 +40,8 @@ class plugin_sco_analytics extends _sco_dx_plugin {
 	}
 
 	function global_header_javascript() {
+		if (defined('IN_MOBILE')) return $ret;
+
 		$ret = '';
 
 		$this->_fix_plugin_setting();
