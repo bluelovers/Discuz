58d011fdaa157961cd8ec175b8511a766993fc65
bluelovers <codelovers@users.sourceforge.net>
2011-10-03 03:38:57 +0800

    .

--- discuzx/upload/source/module/forum/forum_attachment.php
+++ discuzx/upload/source/module/forum/forum_attachment.php
@@ -14,6 +14,11 @@ define('NOROBOT', TRUE);
 @list($_G['gp_aid'], $_G['gp_k'], $_G['gp_t'], $_G['gp_uid'], $_G['gp_tableid']) = daddslashes(explode('|', base64_decode($_G['gp_aid'])));
 
 $aid = intval($_G['gp_aid']);
+
+// bluelovers
+
+// bluelovers
+
 if(!empty($_G['gp_findpost']) && ($attach = DB::fetch_first("SELECT pid, tid FROM ".DB::table('forum_attachment')." WHERE aid='$aid'"))) {
 	dheader('location: forum.php?mod=redirect&goto=findpost&pid='.$attach['pid'].'&ptid='.$attach['tid']);
 }
