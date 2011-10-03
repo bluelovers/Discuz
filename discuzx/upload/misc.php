ced2e233b80fca9ca41db52a02c5e5460e47641a
bluelovers <codelovers@users.sourceforge.net>
2011-10-03 17:04:13 +0800

    define('NOROBOT', false);

--- discuzx/upload/misc.php
+++ discuzx/upload/misc.php
@@ -32,10 +32,10 @@ if(in_array($mod, array('seccode', 'secqaa', 'initsys', 'faq', 'swfupload', 'mob
 }
 
 // bluelovers
-if (!in_array($mod, array(
+if (in_array($mod, array(
 	'tag'
 ))) {
-	$discuz->reject_robot();
+	define('NOROBOT', false);
 }
 
 $discuz->reject_robot();
