8f15602b38167741fa33d566c16adc113297943c
bluelovers <codelovers@users.sourceforge.net>
2011-10-03 17:15:24 +0800

    不明原因 合併分之後 註解變成亂碼

--- discuzx/upload/source/class/class_core.php
+++ discuzx/upload/source/class/class_core.php
@@ -322,9 +322,7 @@ class discuz_core {
 			// bluelovers
 			&& (
 				!defined('NOROBOT')
-				/**
-				 * 允許通過定義 NOROBOT = false 來允許 robots 檢索
-				 */
+				// 允許通過定義 NOROBOT = false 來允許 robots 檢索
 				|| NOROBOT !== false
 			)
 			// bluelovers
