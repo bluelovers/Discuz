e1cd96c6001ce0a29d59dacec37ab3106b52bf33
bluelovers <codelovers@users.sourceforge.net>
2011-10-06 05:52:41 +0800

    discuz_core::$tpl['forum']['topicadmin_action'][$_G['gp_action']] = $_p->_fetch_template($_p->_template('forum/topicadmin_action_author'));

--- discuzx/upload/source/plugin/sco_cpanel/topicadmin/topicadmin_author.php
+++ discuzx/upload/source/plugin/sco_cpanel/topicadmin/topicadmin_author.php
@@ -16,6 +16,14 @@ if (!$_G['uid'] || $_G['adminid'] != 1) {
 $authoridnew = $_G['gp_authoridnew'];
 
 if (!submitcheck('modsubmit')) {
+
+	include_once libfile('class/sco_dx_plugin', 'source', 'extensions/');
+
+	$_p = new _sco_dx_plugin();
+	$_p->identifier = 'sco_cpanel';
+
+	discuz_core::$tpl['forum']['topicadmin_action'][$_G['gp_action']] = $_p->_fetch_template($_p->_template('forum/topicadmin_action_author'));
+
 	include template('forum/topicadmin_action');
 }
 
