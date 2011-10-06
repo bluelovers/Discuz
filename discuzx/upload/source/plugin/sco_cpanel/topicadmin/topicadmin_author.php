ce3d1ad044a091980f964ecde2952fc4e7b80ac4
bluelovers <codelovers@users.sourceforge.net>
2011-10-07 02:53:06 +0800

    .

--- discuzx/upload/source/plugin/sco_cpanel/topicadmin/topicadmin_author.php
+++ discuzx/upload/source/plugin/sco_cpanel/topicadmin/topicadmin_author.php
@@ -106,7 +106,7 @@ if (!submitcheck('modsubmit')) {
 	if ($topiclist) {
 		$_ids = dimplode($topiclist);
 
-		$query = DB::query("SELECT * FROM ".DB::table($posttable)." WHERE tid = '$tid' AND pid IN ($_ids) ORDER BY pid ASC");
+		$query = DB::query("SELECT pid FROM ".DB::table($posttable)." WHERE tid = '$tid' AND pid IN ($_ids) ORDER BY pid ASC");
 
 		$_pids = array();
 		while($_post = DB::fetch($query)) {
