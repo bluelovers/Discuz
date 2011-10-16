abb5b134985e797b235b2fcc9a534b578935cf5f
bluelovers <codelovers@users.sourceforge.net>
2011-10-16 12:24:09 +0800

    Revert "Revert "After""
    This reverts commit 486eaa2722eaaad6d5c47d7acafbea2949b5bf48.

--- discuzx/upload/extensions/hooks/class/class_member.php
+++ discuzx/upload/extensions/hooks/class/class_member.php
@@ -11,9 +11,9 @@ if(!defined('IN_DISCUZ')) {
 
 if (!discuz_core::$plugin_support['Scorpio_Event']) return false;
 
-Scorpio_Hook::add('Class_logging_ctl::on_login:Before_setloginstatus', '_eClass_logging_ctl__on_login_Before_setloginstatus');
+Scorpio_Hook::add('Class_logging_ctl::on_login:After_setloginstatus', '_eClass_logging_ctl__on_login_After_setloginstatus');
 
-function _eClass_logging_ctl__on_login_Before_setloginstatus($_EVENT, $_conf) {
+function _eClass_logging_ctl__on_login_After_setloginstatus($_EVENT, $_conf) {
 	extract($_conf, EXTR_REFS);
 
 	$uid = $result['member']['uid'];
