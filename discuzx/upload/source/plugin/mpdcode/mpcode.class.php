83f8067f7ec1fd334b0f47faa9e1357ca004ccbf
bluelovers <codelovers@users.sourceforge.net>
2011-10-16 18:43:24 +0800

    .

--- discuzx/upload/source/plugin/mpdcode/mpcode.class.php
+++ discuzx/upload/source/plugin/mpdcode/mpcode.class.php
@@ -66,7 +66,7 @@ class plugin_mpdcode_forum extends plugin_mpdcode {
 
 		$EC_level = 'L';
 		$margin = '0';
-		$return .= '<div class="y">手机二维码访问：<br/><a href="' . dhtmlspecialchars($url) . '" target="_blank"><img src="http://chart.apis.google.com/chart?chs=' . $this->wh . 'x' . $this->wh . '&cht=qr&chld=' . $EC_level . '|' . $margin . '&chl=' . $chl . '" alt="QR code"/></a></div>';
+		$return .= '手机二维码访问：<br/><a href="' . dhtmlspecialchars($url) . '" target="_blank"><img src="http://chart.apis.google.com/chart?chs=' . $this->wh . 'x' . $this->wh . '&cht=qr&chld=' . $EC_level . '|' . $margin . '&chl=' . $chl . '" alt="QR code"/></a>';
 
 		return $return;
 	}
