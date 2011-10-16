55f71e92d75325df3f7f7982a692f80b7cc39835
bluelovers <codelovers@users.sourceforge.net>
2011-10-16 18:29:05 +0800

    .

--- discuzx/upload/source/plugin/mpdcode/mpcode.class.php
+++ discuzx/upload/source/plugin/mpdcode/mpcode.class.php
@@ -42,6 +42,37 @@ class plugin_mpdcode_forum extends plugin_mpdcode {
 		return $ret;
 	}
 
+	function ad_ad_a_pr($_conf) {
+		/**
+		 * ad_a_pr/thread/a_pr/3/$postcount
+		 */
+		$parameter = implode('/', $_conf['params']);
+
+		if ($parameter == 'ad_a_pr/thread/a_pr/3/1') {
+			$_conf['content'] .= $this->_output_html();
+		}
+
+		return $_conf['content'];
+	}
+
+	function _output_html() {
+		global $_G, $postlist;
+
+		$_post = reset($postlist);
+
+		if (!$_post['first']) return array();
+
+		$url = $this->_mobile_url($_G['tid']);
+
+		$chl = urlencode($url);
+
+		$EC_level = 'L';
+		$margin = '0';
+		$return .= '<div class="y">手机二维码访问：<br/><a href="' . dhtmlspecialchars($url) . '" target="_blank"><img src="http://chart.apis.google.com/chart?chs=' . $this->wh . 'x' . $this->wh . '&cht=qr&chld=' . $EC_level . '|' . $margin . '&chl=' . $chl . '" alt="QR code"/></a></div>';
+
+		return $return;
+	}
+
 	function _mobile_url($tid = null) {
 		global $_G;
 
