3170090076549c08865c5b6ff1760ef49c393ea6
bluelovers <codelovers@users.sourceforge.net>
2011-10-16 12:08:23 +0800

    extract($_conf, EXTR_REFS);

--- discuzx/upload/extensions/hooks/class/class_member.php
+++ discuzx/upload/extensions/hooks/class/class_member.php
@@ -14,7 +14,7 @@ if (!discuz_core::$plugin_support['Scorpio_Event']) return false;
 Scorpio_Hook::add('Class_logging_ctl::on_login:Before_setloginstatus', '_eClass_logging_ctl__on_login_Before_setloginstatus');
 
 function _eClass_logging_ctl__on_login_Before_setloginstatus($_EVENT, $_conf) {
-
+	extract($_conf, EXTR_REFS);
 }
 
 ?>
