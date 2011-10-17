d1882bdcce40171cf5a686ba36e97ca51f1fc75e
bluelovers <codelovers@users.sourceforge.net>
2011-10-17 13:11:14 +0800

    .

--- discuzx/upload/source/plugin/onexin_sendmail/onexin_sendmail.class.php
+++ discuzx/upload/source/plugin/onexin_sendmail/onexin_sendmail.class.php
@@ -1,5 +1,6 @@
 <?php
-if(!defined('IN_DISCUZ')) {
+
+if (!defined('IN_DISCUZ')) {
 	exit('Access Denied');
 }
 
@@ -15,28 +16,27 @@ class plugin_onexin_sendmail {
 
 	function global_usernav_extra2() {
 		global $_G;
-						
-		if($this->onexin_sendmail) {
-			if(!is_array($_G['setting']['mail'])) {
+
+		if ($this->onexin_sendmail) {
+			if (!is_array($_G['setting']['mail'])) {
 				$_G['setting']['mail'] = unserialize($_G['setting']['mail']);
 			}
-			if($_G['setting']['mail']['mailsend'] == 1) {			
-				ini_set("sendmail_from", $_G['setting']['adminemail']);		
-			}	
+			if ($_G['setting']['mail']['mailsend'] == 1) {
+				ini_set("sendmail_from", $_G['setting']['adminemail']);
+			}
 		}
-								
-		if($this->onexin_verify && empty($_G['member']['emailstatus'])) {	
+
+		if ($this->onexin_verify && empty($_G['member']['emailstatus'])) {
 			//$url = 'home.php?mod=spacecp&ac=profile&op=password&resend='.$_G['uid'].'';
 			$url = 'home.php?mod=spacecp&ac=profile&op=password';
-			return '<span class="pipe">|</span><a id="myprompt" class="new" href="'.$url.'">'.lang('plugin/onexin_sendmail', 'verify_email') .'</a> ';			
+			return '<span class="pipe">|</span><a id="myprompt" class="new" href="' . $url . '">' . lang('plugin/onexin_sendmail', 'verify_email') . '</a> ';
 		}
-	}		
+	}
 
 }
 
 class plugin_onexin_sendmail_forum extends plugin_onexin_sendmail {
 
 }
-	
-	
+
 ?>
