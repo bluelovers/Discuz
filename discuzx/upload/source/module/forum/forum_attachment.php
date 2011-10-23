14906664df1812e50c05025555c350aa7cba7d33
bluelovers <codelovers@users.sourceforge.net>
2011-10-23 15:51:08 +0800

    elseif ($aid)

--- discuzx/upload/source/module/forum/forum_attachment.php
+++ discuzx/upload/source/module/forum/forum_attachment.php
@@ -30,7 +30,7 @@ if (!$aid && is_numeric($_GET['aid'])) {
 	$_G['gp_aid'] = $aid = intval($_GET['aid']);
 
 	$_G['gp_redirectmsg'] = 1;
-} else {
+} elseif ($aid) {
 	// [bugfix] 修正 開啟下載附件的積分策略後造成無限支付的 BUG
 	$_G['forum_attach_aidencode'] = $_GET['aid'];
 }
