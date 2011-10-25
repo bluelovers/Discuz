c6fb4aeeee8c7bdfea50b612a1f0c508014314fb
bluelovers <codelovers@users.sourceforge.net>
2011-10-25 17:08:03 +0800

    public function __construct()

--- discuzx/upload/source/plugin/sco_style/sco_style.class.php
+++ discuzx/upload/source/plugin/sco_style/sco_style.class.php
@@ -9,6 +9,12 @@ include_once libfile('class/sco_dx_plugin', 'source', 'extensions/');
 
 class plugin_sco_style extends _sco_dx_plugin {
 
+	public function __construct() {
+		$this->_init($this->_get_identifier(__CLASS__));
+
+		$this->_this(&$this);
+	}
+
 }
 
 ?>
