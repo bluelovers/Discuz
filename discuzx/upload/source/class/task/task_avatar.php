4167b74664b456e544b8fd5076d0d6cea329cd8d
bluelovers <codelovers@users.sourceforge.net>
2011-11-04 19:58:09 +0800

    var $copyright = '<a href="http://bluelovers.net" target="_blank">Bluelovers Net.</a>';

--- discuzx/upload/source/class/task/task_avatar.php
+++ discuzx/upload/source/class/task/task_avatar.php
@@ -16,7 +16,12 @@ class task_avatar {
 	var $version = '1.0';
 	var $name = 'avatar_name';
 	var $description = 'avatar_desc';
+	/*
 	var $copyright = '<a href="http://www.comsenz.com" target="_blank">Comsenz Inc.</a>';
+	*/
+	// bluelovers
+	var $copyright = '<a href="http://bluelovers.net" target="_blank">Bluelovers Net.</a>';
+	// bluelovers
 	var $icon = '';
 	var $period = '';
 	var $periodtype = 0;
