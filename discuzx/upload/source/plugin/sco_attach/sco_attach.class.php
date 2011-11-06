908e55416aca418eabe6a2cc40b4354520fa5e6e
bluelovers <codelovers@users.sourceforge.net>
2011-11-07 01:06:15 +0800

    class plugin_sco_attach extends _sco_dx_plugin

--- discuzx/upload/source/plugin/sco_attach/sco_attach.class.php
+++ discuzx/upload/source/plugin/sco_attach/sco_attach.class.php
@@ -5,6 +5,14 @@
  * @copyright 2011
  */
 
+if (!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
 
+include_once libfile('class/sco_dx_plugin', 'source', 'extensions/');
+
+class plugin_sco_attach extends _sco_dx_plugin {
+
+}
 
 ?>
