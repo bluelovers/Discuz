a17a9cd938bb771c6330035ea705ce163d6fedeb
bluelovers <codelovers@users.sourceforge.net>
2011-11-06 16:50:17 +0800

    ALTER TABLE `pre_common_plugin` ADD `date_available` INT( 10 ) UNSIGNED NOT NULL DEFAULT '0';

--- discuzx/upload/install/data_sco/table/common_plugin/date.sql
+++ discuzx/upload/install/data_sco/table/common_plugin/date.sql
@@ -1,3 +1,5 @@
 
 ALTER TABLE `pre_common_plugin` ADD `date_install` INT( 10 ) UNSIGNED NOT NULL DEFAULT '0';
 ALTER TABLE `pre_common_plugin` ADD `date_update` INT( 10 ) UNSIGNED NOT NULL DEFAULT '0';
+
+ALTER TABLE `pre_common_plugin` ADD `date_available` INT( 10 ) UNSIGNED NOT NULL DEFAULT '0';
