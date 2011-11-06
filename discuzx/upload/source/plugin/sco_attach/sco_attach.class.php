6a59e8224cbc9ebe2bbadfe5e88ed78027fd61f4
bluelovers <codelovers@users.sourceforge.net>
2011-11-07 05:44:54 +0800

    !empty($attach['description']) &&

--- discuzx/upload/source/plugin/sco_attach/sco_attach.class.php
+++ discuzx/upload/source/plugin/sco_attach/sco_attach.class.php
@@ -58,7 +58,7 @@ class plugin_sco_attach_forum extends plugin_sco_attach {
 		}
 
 		if ($attach) {
-			$_conf['_data_dshowmessage_']['globalvars']['metadescription'] .= ',' . $attach['description'];
+			!empty($attach['description']) && $_conf['_data_dshowmessage_']['globalvars']['metadescription'] .= ',' . $attach['description'];
 		}
 
 		if (!empty($_G['forum_attach_filename'])) {
