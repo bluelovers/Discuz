f4834dcc0016ebe0ba9fe6b49e391c238385e049
bluelovers <codelovers@users.sourceforge.net>
2011-11-07 05:46:33 +0800

    .

--- discuzx/upload/source/plugin/sco_attach/sco_attach.class.php
+++ discuzx/upload/source/plugin/sco_attach/sco_attach.class.php
@@ -59,6 +59,9 @@ class plugin_sco_attach_forum extends plugin_sco_attach {
 
 		if ($attach) {
 			!empty($attach['description']) && $_conf['_data_dshowmessage_']['globalvars']['metadescription'] .= ',' . $attach['description'];
+
+			$tid = $attach['tid'];
+			$pid = $attach['pid'];
 		}
 
 		if (!empty($_G['forum_attach_filename'])) {
