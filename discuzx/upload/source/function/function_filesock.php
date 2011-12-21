94aba047caa97f6375528784083d72707e020e60
bluelovers <codelovers@users.sourceforge.net>
2011-12-22 04:13:25 +0800

    FIX 完善dfsockopen函数

--- discuzx/upload/source/function/function_filesock.php
+++ discuzx/upload/source/function/function_filesock.php
@@ -4,7 +4,7 @@
  *      [Discuz!] (C)2001-2099 Comsenz Inc.
  *      This is NOT a freeware, use is subject to license terms
  *
- *      $Id: function_filesock.php 17199 2010-09-26 07:12:20Z cnteacher $
+ *      $Id: function_filesock.php 26500 2011-12-14 04:36:28Z svn_project_zhangjie $
  */
 
 if(!defined('IN_DISCUZ')) {
@@ -14,38 +14,47 @@ if(!defined('IN_DISCUZ')) {
 function _dfsockopen($url, $limit = 0, $post = '', $cookie = '', $bysocket = FALSE, $ip = '', $timeout = 15, $block = TRUE) {
 	$return = '';
 	$matches = parse_url($url);
+	$scheme = $matches['scheme'];
 	$host = $matches['host'];
 	$path = $matches['path'] ? $matches['path'].($matches['query'] ? '?'.$matches['query'] : '') : '/';
 	$port = !empty($matches['port']) ? $matches['port'] : 80;
 
 	if($post) {
 		$out = "POST $path HTTP/1.0\r\n";
-		$out .= "Accept: */*\r\n";
-		$out .= "Accept-Language: zh-cn\r\n";
-		$out .= "Content-Type: application/x-www-form-urlencoded\r\n";
-		$out .= "User-Agent: $_SERVER[HTTP_USER_AGENT]\r\n";
-		$out .= "Host: $host\r\n";
-		$out .= 'Content-Length: '.strlen($post)."\r\n";
-		$out .= "Connection: Close\r\n";
-		$out .= "Cache-Control: no-cache\r\n";
-		$out .= "Cookie: $cookie\r\n\r\n";
-		$out .= $post;
+		$header = "Accept: */*\r\n";
+		$header .= "Accept-Language: zh-cn\r\n";
+		$header .= "Content-Type: application/x-www-form-urlencoded\r\n";
+		$header .= "User-Agent: $_SERVER[HTTP_USER_AGENT]\r\n";
+		$header .= "Host: $host\r\n";
+		$header .= 'Content-Length: '.strlen($post)."\r\n";
+		$header .= "Connection: Close\r\n";
+		$header .= "Cache-Control: no-cache\r\n";
+		$header .= "Cookie: $cookie\r\n\r\n";
+		$out .= $header.$post;
 	} else {
 		$out = "GET $path HTTP/1.0\r\n";
-		$out .= "Accept: */*\r\n";
-		$out .= "Accept-Language: zh-cn\r\n";
-		$out .= "User-Agent: $_SERVER[HTTP_USER_AGENT]\r\n";
-		$out .= "Host: $host\r\n";
-		$out .= "Connection: Close\r\n";
-		$out .= "Cookie: $cookie\r\n\r\n";
+		$header = "Accept: */*\r\n";
+		$header .= "Accept-Language: zh-cn\r\n";
+		$header .= "User-Agent: $_SERVER[HTTP_USER_AGENT]\r\n";
+		$header .= "Host: $host\r\n";
+		$header .= "Connection: Close\r\n";
+		$header .= "Cookie: $cookie\r\n\r\n";
+		$out .= $header;
 	}
 
-	if(function_exists('fsockopen')) {
-		$fp = @fsockopen(($ip ? $ip : $host), $port, $errno, $errstr, $timeout);
-	} elseif(function_exists('pfsockopen')) {
-		$fp = @pfsockopen(($ip ? $ip : $host), $port, $errno, $errstr, $timeout);
-	} else {
-		$fp = '';
+	$fpflag = 0;
+	if(!$fp = @fsocketopen(($ip ? $ip : $host), $port, $errno, $errstr, $timeout)) {
+		$context = array(
+			'http' => array(
+				'method' => $post ? 'POST' : 'GET',
+				'header' => $header,
+				'content' => $post,
+				'timeout' => $timeout,
+			),
+		);
+		$context = stream_context_create($context);
+		$fp = @fopen($scheme.'://'.($ip ? $ip : $host).':'.$port.$path, 'b', false, $context);
+		$fpflag = 1;
 	}
 
 	if(!$fp) {
@@ -56,7 +65,7 @@ function _dfsockopen($url, $limit = 0, $post = '', $cookie = '', $bysocket = FAL
 		@fwrite($fp, $out);
 		$status = stream_get_meta_data($fp);
 		if(!$status['timed_out']) {
-			while (!feof($fp)) {
+			while (!feof($fp) && !$fpflag) {
 				if(($header = @fgets($fp)) && ($header == "\r\n" ||  $header == "\n")) {
 					break;
 				}
