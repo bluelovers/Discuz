ad686010b8295dad9c4bf2dd5a765e7618237203
bluelovers <codelovers@users.sourceforge.net>
2012-05-01 12:44:24 +0800

    ../upload/api/manyou/Service/Client/Favorite.php

--- discuzx/upload/api/manyou/Service/Client/Favorite.php
+++ discuzx/upload/api/manyou/Service/Client/Favorite.php
@@ -3,7 +3,7 @@
  *      [Discuz!] (C)2001-2099 Comsenz Inc.
  *      This is NOT a freeware, use is subject to license terms
  *
- *      $Id: Favorite.php 29263 2012-03-31 05:45:08Z yexinhao $
+ *      $Id: Favorite.php 29721 2012-04-26 07:01:08Z zhengqingpeng $
  */
 
 if(!defined('IN_DISCUZ')) {
@@ -31,35 +31,28 @@ class Cloud_Service_Client_Favorite extends Cloud_Service_Client_Restful {
 	}
 
 	public function add($siteUid, $pkId, $id, $idType, $title, $description, $dateline) {
-		$openId = $this->getUserOpenId($siteUid);
-		if($openId) {
-			$_params = array(
-					'openid' => $openId,
-					'sSiteUid' => $siteUid,
-					'pkId' => $pkId,
-					'fromId' => $id,
-					'fromIdType' => $idType,
-					'title' => $title,
-					'description' => $description,
-					'dateline' => $dateline
+		$_params = array(
+				'openid' => $this->getUserOpenId($siteUid),
+				'sSiteUid' => $siteUid,
+				'pkId' => $pkId,
+				'fromId' => $id,
+				'fromIdType' => $idType,
+				'title' => $title,
+				'description' => $description,
+				'dateline' => $dateline,
+				'deviceToken' => $this->getUserDeviceToken($siteUid)
 			);
-			return $this->_callMethod('connect.discuz.favorite.add', $_params);
-		}
-		return false;
+		return $this->_callMethod('connect.discuz.favorite.add', $_params);
 	}
 
 	public function remove($siteUid, $pkIds, $dateline) {
-		$openId = $this->getUserOpenId($siteUid);
-		if($openId) {
-			$_params = array(
-					'openid' => $openId,
-					'sSiteUid' => $siteUid,
-					'pkIds' => $pkIds,
-					'dateline' => $dateline
+		$_params = array(
+				'openid' => $this->getUserOpenId($siteUid),
+				'sSiteUid' => $siteUid,
+				'pkIds' => $pkIds,
+				'dateline' => $dateline
 			);
-			return $this->_callMethod('connect.discuz.favorite.remove', $_params);
-		}
-		return false;
+		return $this->_callMethod('connect.discuz.favorite.remove', $_params);
 	}
 	protected function _callMethod($method, $args) {
 		try {
