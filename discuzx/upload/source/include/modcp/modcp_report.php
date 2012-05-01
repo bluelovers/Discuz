f4946be01650f9425a3b98a1d1e21b63a5076705
bluelovers <codelovers@users.sourceforge.net>
2012-05-01 14:33:24 +0800

    20120427
    path: discuzx/upload/source/include/modcp/modcp_report.php

--- discuzx/upload/source/include/modcp/modcp_report.php
+++ discuzx/upload/source/include/modcp/modcp_report.php
@@ -4,7 +4,7 @@
  *      [Discuz!] (C)2001-2099 Comsenz Inc.
  *      This is NOT a freeware, use is subject to license terms
  *
- *      $Id: modcp_report.php 29236 2012-03-30 05:34:47Z chenmengshu $
+ *      $Id: modcp_report.php 29666 2012-04-24 08:07:56Z zhengqingpeng $
  */
 
 if(!defined('IN_DISCUZ') || !defined('IN_MODCP')) {
@@ -12,6 +12,7 @@ if(!defined('IN_DISCUZ') || !defined('IN_MODCP')) {
 }
 if(!empty($_G['fid'])) {
 	$curcredits = $_G['setting']['creditstransextra'][8] ? $_G['setting']['creditstransextra'][8] : $_G['setting']['creditstrans'];
+	$report_reward = unserialize($_G['setting']['report_reward']);
 	if(submitcheck('reportsubmit')) {
 		if($_GET['reportids']) {
 			foreach($_GET['reportids'] as $reportid) {
@@ -22,6 +23,9 @@ if(!empty($_G['fid'])) {
 						$msg = !empty($_GET['msg'][$reportid]) ? '<br />'.dhtmlspecialchars($_GET['msg'][$reportid]) : '';
 						if(!empty($_GET['creditsvalue'][$reportid])) {
 							$credittag = $_GET['creditsvalue'][$reportid] > 0 ? '+' : '';
+							if($report_reward['max'] < $_GET['creditsvalue'][$reportid] || $_GET['creditsvalue'][$reportid] < $report_reward['min']) {
+								showmessage('quickclear_noperm', "$cpscript?mod=modcp&action=report&fid=$_G[fid]");
+							}
 							$creditchange = '<br />'.lang('forum/misc', 'report_msg_your').$_G['setting']['extcredits'][$curcredits]['title'].'&nbsp;'.$credittag.$_GET['creditsvalue'][$reportid];
 							updatemembercount($uid, array($curcredits => intval($_GET['creditsvalue'][$reportid])), true, 'RPC', $reportid);
 						}
@@ -37,7 +41,6 @@ if(!empty($_G['fid'])) {
 		}
 	}
 	$rewardlist = '';
-	$report_reward = unserialize($_G['setting']['report_reward']);
 	$offset = abs(ceil(($report_reward['max'] - $report_reward['min']) / 10));
 	if($report_reward['max'] > $report_reward['min']) {
 		for($vote = $report_reward['max']; $vote >= $report_reward['min']; $vote -= $offset) {
