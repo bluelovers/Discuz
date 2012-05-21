4da6587f6e877a329b3c9515265e211ed0717b5a
bluelovers <codelovers@users.sourceforge.net>
2012-05-22 05:10:00 +0800

    Discuz! X2.5 正式版 20120518 (Step: 18)
    Step: 18
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	discuzx/upload/source/discuz_version.php

--- discuzx/upload/source/discuz_version.php
+++ discuzx/upload/source/discuz_version.php
@@ -4,7 +4,7 @@
  *      [Discuz!] (C)2001-2099 Comsenz Inc.
  *      This is NOT a freeware, use is subject to license terms
  *
- *      $Id: discuz_version.php 29767 2012-04-27 02:56:00Z cnteacher $
+ *      $Id: discuz_version.php 30267 2012-05-18 01:41:05Z svn_project_zhangjie $
  */
 
 if(!defined('IN_DISCUZ')) {
@@ -13,8 +13,8 @@ if(!defined('IN_DISCUZ')) {
 
 if(!defined('DISCUZ_VERSION')) {
 	define('DISCUZ_VERSION', 'X2.5');
-	define('DISCUZ_RELEASE', '20120427');
-	define('DISCUZ_FIXBUG', '25000000');
+	define('DISCUZ_RELEASE', '20120518');
+	define('DISCUZ_FIXBUG', '25000001');
 }
 
 ?>
