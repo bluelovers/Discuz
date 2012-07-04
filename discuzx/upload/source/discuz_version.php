deca5ec59911c6a93024cffa1c4dde5d97825064
bluelovers <codelovers@users.sourceforge.net>
2012-07-04 10:25:14 +0800

    Discuz! X2.5  R20120701 (Step: 21)
    Step: 21
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	discuzx/upload/source/discuz_version.php

--- discuzx/upload/source/discuz_version.php
+++ discuzx/upload/source/discuz_version.php
@@ -4,7 +4,7 @@
  *      [Discuz!] (C)2001-2099 Comsenz Inc.
  *      This is NOT a freeware, use is subject to license terms
  *
- *      $Id: discuz_version.php 30498 2012-05-31 06:15:34Z cnteacher $
+ *      $Id: discuz_version.php 30925 2012-07-02 05:47:03Z cnteacher $
  */
 
 if(!defined('IN_DISCUZ')) {
@@ -13,7 +13,7 @@ if(!defined('IN_DISCUZ')) {
 
 if(!defined('DISCUZ_VERSION')) {
 	define('DISCUZ_VERSION', 'X2.5');
-	define('DISCUZ_RELEASE', '20120601');
+	define('DISCUZ_RELEASE', '20120701');
 	define('DISCUZ_FIXBUG', '25000002');
 }
 
